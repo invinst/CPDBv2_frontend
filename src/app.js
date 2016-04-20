248927f218a20fdf0ba58a9724138fef31d2cc1e
ea-open-source <open-source@eastagile.com>
2016-04-20 17:12:51 +0700

    Added circle.yml

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class HelloWorld extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, foo</h1>
+        <h1>Hello, { this.props.name }</h1>
       </div>
     );
   }
 }
 
-// // detect test runner, there's for sure a better way
-// if (typeof window.it !== 'function')
-//   ReactDOM.render(<HelloWorld name='World' />, document.getElementById('App'));
+HelloWorld.propTypes = {
+  name: React.PropTypes.string.isRequired
+};
