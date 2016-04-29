edc3e910c12b68f751bf3b3ae6f66b631fb1cde2
ea-open-source <open-source@eastagile.com>
2016-04-29 11:03:24 +0700

    rename ArticleTitle component to ArticleContent

---
+++ src/js/components/common/article-content.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+import Radium from 'radium';
+
+import { contentStyle } from 'components/common/article-content.style';
+
+
+class ArticleContent extends React.Component {
+  render() {
+    return (<p style={ contentStyle }>
+      { this.props.children }
+    </p>);
+  }
+}
+
+ArticleContent.propTypes = {
+  children: PropTypes.node
+};
+
+export default Radium(ArticleContent);
--- src/js/components/story-small.js
+++ src/js/components/story-small.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 import Radium from 'radium';
 
 import ArticleHeader from 'components/common/article-header';
-import ArticleTitle from 'components/common/article-title';
+import ArticleContent from 'components/common/article-content';
 import ResponsiveStyleComponent, {DESKTOP, TABLET, MOBILE} from 'components/responsive-style-component';
 import {wrapperStyle, tabletWrapperStyle, mobileWrapperStyle} from 'components/story-small.style';
 
@@ -25,7 +25,7 @@ class StorySmall extends ResponsiveStyleComponent {
   renderWithResponsiveStyle(style) {
     return (<div style={ style.wrapper }>
       <ArticleHeader>{ this.props.story.paper }</ArticleHeader>
-      <ArticleTitle>{ this.props.story.title }</ArticleTitle>
+      <ArticleContent>{ this.props.story.title }</ArticleContent>
     </div>);
   }
 }
