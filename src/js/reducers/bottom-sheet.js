4aa8c8508145d41dc827bdab57624dd8f5a1f5ec
ea-open-source <open-source@eastagile.com>
2016-05-19 11:47:52 +0700

    [f] 695 - disable body scroll when bottom sheet open
    [#119623695] https://www.pivotaltracker.com/story/show/119623695

--- src/js/reducers/bottom-sheet.js
+++ src/js/reducers/bottom-sheet.js
@@ -1,9 +1,16 @@
 import { handleActions } from 'redux-actions';
 
 import { OPEN_BOTTOM_SHEET, CLOSE_BOTTOM_SHEET } from 'actions/bottom-sheet';
+import { disableBodyScroll, enableBodyScroll } from 'utils/dom';
 
 
 export default handleActions({
-  [OPEN_BOTTOM_SHEET]: (state, action) => ({ open: true }),
-  [CLOSE_BOTTOM_SHEET]: (state, action) => ({ open: false })
+  [OPEN_BOTTOM_SHEET]: (state, action) => {
+    disableBodyScroll();
+    return { open: true };
+  },
+  [CLOSE_BOTTOM_SHEET]: (state, action) => {
+    enableBodyScroll();
+    return { open: false };
+  }
 }, { open: false });
