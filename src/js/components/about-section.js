9fadb6662002365692a1cbd8693f0556798a0288
ea-open-source <open-source@eastagile.com>
2016-05-24 11:39:38 +0700

    [f] 971 - refactor about-section, add renderable test
    [#119624971] https://www.pivotaltracker.com/story/show/119624971

--- src/js/components/about-section.js
+++ src/js/components/about-section.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 
 import ArticleSmall from 'components/common/article-small';
 import ArticleContent from 'components/common/article-content';
-import UnderlinedLink from 'components/common/underlined-link';
 import ResponsiveComponent from 'components/responsive/responsive-component';
 import {
   articleTabletStyle, articleDesktopStyle
@@ -21,20 +20,6 @@ export default class AboutSection extends ResponsiveComponent {
           The information and stories we have collected here are intended
           as a resource for public oversight. Our aim is to create a new
           model of accountability between officers and citizens.
-        </ArticleContent>
-      </div>
-    );
-  }
-
-  renderCollaborationArticle() {
-    return (
-      <div>
-        <ArticleContent>
-          We are collecting and publishing information that sheds light on police misconduct.
-        </ArticleContent>
-        <ArticleContent>
-          If you have documents or datasets you would like to publish,
-          please <UnderlinedLink href='mailto:records@invisibleinstitute.com'>email us.</UnderlinedLink>
         </ArticleContent>
       </div>
     );
---
+++ test/components/collaborate-section.js
@@ -0,0 +1,8 @@
+import CollaborateSection from 'components/collaborate-section';
+
+
+describe('CollaborateSection component', function () {
+  it('should be renderable', function () {
+    CollaborateSection.should.be.renderable();
+  });
+});
