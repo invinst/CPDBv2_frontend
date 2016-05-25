5e0b533189fde9f6a907dff6f3d15f4651fb382d
ea-open-source <open-source@eastagile.com>
2016-05-25 17:22:08 +0700

    [f] 185 - readd responsive fixed width to landing page
    [#119625185] https://www.pivotaltracker.com/story/show/119625185

--- src/js/components/landing-page.js
+++ src/js/components/landing-page.js
@@ -5,6 +5,7 @@ import FAQSection from 'components/faq/faq-section';
 import TwitterEmbeddedTimeline from 'components/twitter-embedded-timeline';
 import AboutSection from 'components/about-section';
 import HeroSection from 'components/hero-section';
+import ResponsiveFixedWidthComponent from 'components/responsive/responsive-fixed-width-component';
 import { borderTop, leftColumnStyle } from './landing-page.style';
 
 
@@ -12,20 +13,21 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div>
-        <HeroSection />
-
-        <StoriesContainer store={ this.props.store }/>
-        <div className='pure-g' style={ borderTop }>
-          <div className='pure-u-de-3-5 pure-u-ta-1-2 pure-u-mo-1-2'>
-            <div style={ leftColumnStyle }>
-              <FAQSection/>
-              <AboutSection style={ borderTop }/>
+        <ResponsiveFixedWidthComponent>
+          <HeroSection />
+          <StoriesContainer store={ this.props.store }/>
+          <div className='pure-g' style={ borderTop }>
+            <div className='pure-u-de-3-5 pure-u-ta-1-2 pure-u-mo-1-2'>
+              <div style={ leftColumnStyle }>
+                <FAQSection/>
+                <AboutSection style={ borderTop }/>
+              </div>
+            </div>
+            <div className='pure-u-de-2-5 pure-u-ta-1-2 pure-u-mo-1-2'>
+              <TwitterEmbeddedTimeline/>
             </div>
           </div>
-          <div className='pure-u-de-2-5 pure-u-ta-1-2 pure-u-mo-1-2'>
-            <TwitterEmbeddedTimeline/>
-          </div>
-        </div>
+        </ResponsiveFixedWidthComponent>
       </div>
     );
   }
