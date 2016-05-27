d0bcba385ab62b0cd4ce4cad15dfc6bec75eaad0
ea-open-source <open-source@eastagile.com>
2016-05-27 15:46:04 +0700

    [c] 471 - fix wrong gulp task name global module
    [#120350471] https://www.pivotaltracker.com/story/show/120350471

--- nightwatch.global.js
+++ nightwatch.global.js
@@ -5,7 +5,7 @@ require('./gulpfile.js');
 
 module.exports = {
   before: function (done) {
-    gulp.start('build-js-test', function () {
+    gulp.start('build-js-live-test', function () {
       browserSync.init({
         notify: false,
         port: 9100,
