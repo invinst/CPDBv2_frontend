14fd942367957cb3fc29fbc7afcde5824d672fe2
ea-open-source <open-source@eastagile.com>
2016-05-30 10:32:59 +0700

    [c] 051 - add trailing slashes to resources
    [#119969051] https://www.pivotaltracker.com/story/show/119969051

--- src/js/actions/faq-app.js
+++ src/js/actions/faq-app.js
@@ -2,7 +2,7 @@ export const FAQS_REQUEST_START = 'FAQS_REQUEST_START';
 export const FAQS_REQUEST_SUCCESS = 'FAQS_REQUEST_SUCCESS';
 export const FAQS_REQUEST_FAILURE = 'FAQS_REQUEST_FAILURE';
 
-export const FAQS_API_URL = 'faqs';
+export const FAQS_API_URL = 'faqs/';
 
 export const requestFAQs = (params, adapter) => ({
   types: [FAQS_REQUEST_START, FAQS_REQUEST_SUCCESS, FAQS_REQUEST_FAILURE],
--- src/js/actions/story-app.js
+++ src/js/actions/story-app.js
@@ -2,7 +2,7 @@ export const STORIES_REQUEST_START = 'STORIES_REQUEST_START';
 export const STORIES_REQUEST_SUCCESS = 'STORIES_REQUEST_SUCCESS';
 export const STORIES_REQUEST_FAILURE = 'STORIES_REQUEST_FAILURE';
 
-export const STORIES_API_URL = 'stories';
+export const STORIES_API_URL = 'stories/';
 
 export const requestStories = (params, adapter) => ({
   types: [STORIES_REQUEST_START, STORIES_REQUEST_SUCCESS, STORIES_REQUEST_FAILURE],
