73b958c337c90862203ac89365914993e31bb8f2
ea-open-source <open-source@eastagile.com>
2016-07-08 10:53:28 +0700

    [f] 261: Add real animation
    [#124714261] https://www.pivotaltracker.com/story/show/124714261

--- src/js/components/stories-page/loading-indicator.js
+++ src/js/components/stories-page/loading-indicator.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+import { loadingIndicatorStyle } from './loading-indicator.style.js';
+
 
 export default class LoadingIndicator extends Component {
   render() {
-    return <div style={ { position: 'absolute', height: '100px' } }>Loading...</div>;
+    return <div className='fa fa-spinner fa-pulse' style={ loadingIndicatorStyle }></div>;
   }
 }
---
+++ src/js/components/stories-page/loading-indicator.style.js
@@ -0,0 +1,6 @@
+export const loadingIndicatorStyle = {
+  position: 'absolute',
+  width: '100%',
+  fontSize: '30pt',
+  textAlign: 'center'
+};
