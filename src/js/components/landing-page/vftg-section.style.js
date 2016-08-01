8b2be3e98a9f46773e299c68d0c8ce3cd9e56e71
ea-open-source <open-source@eastagile.com>
2016-08-01 09:18:13 +0700

    [f] 037 - VTFG component on landing page
    [#121568037] https://www.pivotaltracker.com/story/show/121568037

---
+++ src/js/components/landing-page/vftg-section.style.js
@@ -0,0 +1,91 @@
+import { softBlackColor, codGrayColor, lightMineShaftColor, sanFranciscoTextFamily } from 'utils/styles';
+
+
+export const headerBlockStyle = {
+  marginBottom: '16px'
+};
+
+export const headerStyle = {
+  fontFamily: sanFranciscoTextFamily,
+  fontSize: '13px',
+  fontWeight: 600,
+  color: codGrayColor
+};
+
+export const dateStyle = {
+  fontFamily: sanFranciscoTextFamily,
+  fontSize: '13px',
+  color: lightMineShaftColor,
+  marginLeft: '6px'
+};
+
+export const textStyle = {
+  fontFamily: sanFranciscoTextFamily,
+  fontSize: '20px',
+  fontWeight: 400,
+  color: softBlackColor
+};
+
+export const newsWrapperStyle = {
+  paddingLeft: '48px',
+  marginBottom: '16px'
+};
+
+export const textInputStyle = {
+  height: '38px',
+  backgroundColor: 'white',
+  fontFamily: sanFranciscoTextFamily,
+  fontSize: '13px',
+  border: 0,
+  outline: 0,
+  paddingLeft: '8px',
+  paddingRight: 0,
+  width: 'calc(100% - 8px)'
+};
+
+export const subscribeBtnStyle = {
+  backgroundColor: 'rgba(0, 94, 243, 0.23)',
+  width: '79px',
+  height: '26px',
+  fontWeight: 500,
+  fontSize: '13px',
+  lineHeight: '20px',
+  verticalAlign: 'middle',
+  borderRadius: '3px',
+  border: 'none'
+};
+
+export const formWrapperStyle = {
+  paddingLeft: '48px'
+};
+
+export const formHeaderBlockStyle = {
+  marginBottom: '22px',
+  fontFamily: sanFranciscoTextFamily,
+  fontSize: '13px',
+  fontWeight: 700,
+  color: softBlackColor,
+  position: 'relative'
+};
+
+export const iconStyle = {
+  position: 'absolute',
+  width: '26px',
+  height: '22px',
+  left: '-48px'
+};
+
+export const formHeaderStyle = {
+  boxShadow: '0px 2px 0px 0px #adadad',
+  borderBottom: '4px solid rgba(35, 31, 32, 0.2)',
+  paddingBottom: '3px'
+};
+
+export const formActionBlockStyle = {
+  marginTop: '16px',
+  textAlign: 'right'
+};
+
+export const vftgWrapperStyle = {
+  padding: '30px 32px 22px 25px'
+};
