a0015932bf664009d9514c95aa09c5a6e8acbaf0
ea-open-source <open-source@eastagile.com>
2016-10-03 12:29:50 +0700

    [c] add react-perf on local only

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Perf from 'react-addons-perf';
 import TwitterWidgetsLoader from 'twitter-widgets';
 
 import 'polyfill';
 import RouterRoot from 'components/router-root';
 
-
-global.Perf = Perf;
+if (global.DEVELOPMENT) {
+  global.Perf = require('react-addons-perf');
+}
 
 TwitterWidgetsLoader.load(function (twttr) {
   global.twttr = twttr;
