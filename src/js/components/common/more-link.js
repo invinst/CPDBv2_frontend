c94da6441d44182878f19ff40292f7be2daf219f
ea-open-source <open-source@eastagile.com>
2016-10-07 15:35:09 +0700

    [f] 519d - refactor MoreLink component
    [#131797849] https://www.pivotaltracker.com/story/show/131797849

--- src/js/components/common/more-link.js
+++ src/js/components/common/more-link.js
@@ -3,31 +3,23 @@ import React, { Component, PropTypes } from 'react';
 import Hoverable from 'components/common/higher-order/hoverable';
 import Link from 'components/common/react-router-link';
 import ConfiguredRadium from 'utils/configured-radium';
-import {
-  baseStyle, underlineStyle, underlineWrapperStyle, underlineHoverStyle, baseHoverStyle
-} from './more-link.style';
+import UnderlineText from 'components/common/underline-text';
+import { linkStyle } from './more-link.style';
 
 
 class MoreLink extends Component {
   render() {
     const { to, children, style, href, hovering, onClick } = this.props;
-    const _baseStyle = style.base || {};
-    const _underlineStyle = style.underline || {};
 
     if (to) {
       return (
         <Link to={ to }
           onMouseOver={ this.handleMouseOver }
           onMouseOut={ this.handleMouseOut }
-          className='link--transition'
-          style={ hovering ? [baseHoverStyle, _baseStyle.hover] : [baseStyle, _baseStyle.base] }>
-          { children }
-          <span style={ underlineWrapperStyle }>
-            <span className='link--transition'
-              style={ hovering ?
-                [underlineHoverStyle, _underlineStyle.hover] :
-                [underlineStyle, _underlineStyle.base] }/>
-          </span>
+          style={ linkStyle }>
+          <UnderlineText hovering={ hovering } style={ style }>
+            { children }
+          </UnderlineText>
         </Link>
       );
     }
@@ -37,15 +29,10 @@ class MoreLink extends Component {
         onMouseOver={ this.handleMouseOver }
         onMouseOut={ this.handleMouseOut }
         onClick={ onClick }
-        className='link--transition'
-        style={ hovering ? [baseHoverStyle, _baseStyle.hover] : [baseStyle, _baseStyle.base] }>
-        { children }
-        <span style={ underlineWrapperStyle }>
-          <span className='link--transition'
-            style={ hovering ?
-                [underlineHoverStyle, _underlineStyle.hover] :
-                [underlineStyle, _underlineStyle.base] }/>
-        </span>
+        style={ linkStyle }>
+        <UnderlineText hovering={ hovering } style={ style }>
+          { children }
+        </UnderlineText>
       </a>
     );
   }
@@ -55,16 +42,7 @@ MoreLink.propTypes = {
   to: PropTypes.string,
   href: PropTypes.string,
   children: PropTypes.node,
-  style: PropTypes.shape({
-    base: PropTypes.shape({
-      base: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-      hover: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
-    }),
-    underline: PropTypes.shape({
-      base: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-      hover: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
-    })
-  }),
+  style: PropTypes.object,
   hovering: PropTypes.bool,
   onClick: PropTypes.func
 };
---
+++ src/js/components/common/underline-text.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+
+import {
+  baseStyle, underlineStyle, underlineWrapperStyle, underlineHoverStyle,
+  baseHoverStyle
+} from './underline-text.style';
+
+
+export default class UnderlineText extends Component {
+  render() {
+    const { hovering, children, style } = this.props;
+    const _baseStyle = style.base || {};
+    const _underlineStyle = style.underline || {};
+
+    return (
+      <span
+        className='link--transition'
+        style={ hovering ?
+          { ...baseHoverStyle, ..._baseStyle.hover } :
+          { ...baseStyle, ..._baseStyle.base }
+        }>
+        { children }
+        <span style={ underlineWrapperStyle }>
+          <span className='link--transition'
+            style={ hovering ?
+              { ...underlineHoverStyle, ..._underlineStyle.hover } :
+              { ...underlineStyle, ..._underlineStyle.base }
+            }/>
+        </span>
+      </span>
+    );
+  }
+}
+
+UnderlineText.propTypes = {
+  hovering: PropTypes.bool,
+  children: PropTypes.node,
+  style: PropTypes.shape({
+    base: PropTypes.shape({
+      base: PropTypes.object,
+      hover: PropTypes.object
+    }),
+    underline: PropTypes.shape({
+      base: PropTypes.object,
+      hover: PropTypes.object
+    })
+  })
+};
