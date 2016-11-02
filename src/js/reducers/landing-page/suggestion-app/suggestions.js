d31f6c983c7cedccc76043a0b982e4d3b20ae9db
ea-open-source <open-source@eastagile.com>
2016-11-02 09:52:32 +0700

    [f] 161 - Mapping with backend api
    [#133408161] https://www.pivotaltracker.com/story/show/133408161

--- src/js/reducers/landing-page/suggestion-app/suggestions.js
+++ src/js/reducers/landing-page/suggestion-app/suggestions.js
@@ -1,12 +1,21 @@
 import { handleActions } from 'redux-actions';
+import { map, reduce } from 'lodash';
 
 import {
   SUGGESTION_REQUEST_START, SUGGESTION_REQUEST_SUCCESS, SUGGESTION_REQUEST_FAILURE
 } from 'actions/landing-page/suggestion-action';
 
 
+const SUGGESTION_MAP = ['neighborhood', 'officer badge', 'officer name'];
+
 export default handleActions({
   [SUGGESTION_REQUEST_START]: (state, action) => ([]),
-  [SUGGESTION_REQUEST_SUCCESS]: (state, action) => (action.payload.options),
+  [SUGGESTION_REQUEST_SUCCESS]: (state, action) => {
+    return reduce(
+      map(SUGGESTION_MAP, (contentType) => (action.payload[contentType])),
+      (memo, suggestion) => (
+        memo.concat(suggestion[0].options)
+      ), [])
+  },
   [SUGGESTION_REQUEST_FAILURE]: (state, action) => ([])
 }, []);
