a098dec84dd1f4a8559c5d27cf848cc48ef1005d
ea-open-source <open-source@eastagile.com>
2016-12-13 13:10:30 +0700

    [f] 145 - Make hero section display responsively
    [#135433145] https://www.pivotaltracker.com/story/show/135433145

---
+++ test/components/landing-page/hero-section/sunburst-graphic.js
@@ -0,0 +1,8 @@
+import SunburstGraphic from 'components/landing-page/hero-section/sunburst-graphic';
+
+
+describe('SunburstGraphic component', function () {
+  it('should renderable', function () {
+    SunburstGraphic.should.be.renderable();
+  });
+});
