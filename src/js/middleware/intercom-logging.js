71b44dc4a44cc68408aa247ad7fb01a9fe5b17e3
ea-open-source <open-source@eastagile.com>
2017-01-06 16:53:18 +0700

    [f] 887b - Track openBottomSheetWithFAQ action too

--- src/js/middleware/intercom-logging.js
+++ src/js/middleware/intercom-logging.js
@@ -1,6 +1,11 @@
-import { OPEN_BOTTOM_SHEET_WITH_REPORT } from 'actions/bottom-sheet';
+import {
+  OPEN_BOTTOM_SHEET_WITH_REPORT,
+  OPEN_BOTTOM_SHEET_WITH_FAQ
+} from 'actions/bottom-sheet';
 import { EXPAND_FAQ } from 'actions/faq-page/index';
 import { trackClickedReportingItem, trackClickedFaqItem } from 'utils/intercom';
+import { getField, plainTextValueToString, multilineTextValueToArray } from 'utils/draft';
+import _ from 'lodash';
 
 
 export default store => next => action => {
@@ -12,6 +17,14 @@ export default store => next => action => {
   if (action.type === EXPAND_FAQ) {
     const { id, question, answer } = action.payload;
     trackClickedFaqItem(id, question, answer);
+  }
+
+  if (action.type === OPEN_BOTTOM_SHEET_WITH_FAQ) {
+    const faqs = store.getState().faqs;
+    const faq = _.find(faqs, ['id', action.payload]);
+    const question = plainTextValueToString(getField(faq.fields, 'question').value);
+    const answer = multilineTextValueToArray(getField(faq.fields, 'answer').value).join('\n');
+    trackClickedFaqItem(faq.id, question, answer);
   }
 
   return next(action);
--- test/middleware/intercom-logging.js
+++ test/middleware/intercom-logging.js
@@ -1,7 +1,7 @@
 import { stub } from 'sinon';
 
 import IntercomLoggingMiddleware from 'middleware/intercom-logging';
-import { openBottomSheetWithReport } from 'actions/bottom-sheet';
+import { openBottomSheetWithReport, openBottomSheetWithFAQ } from 'actions/bottom-sheet';
 import { expandFAQ } from 'actions/faq-page/index';
 import * as intercomUtils from 'utils/intercom';
 
@@ -32,6 +32,44 @@ describe('intercomLoggingMiddleware', function () {
     IntercomLoggingMiddleware({})(action => dispatched = action)(dispatchAction);
     intercomUtils.trackClickedFaqItem.calledOnce.should.equal(true);
     intercomUtils.trackClickedFaqItem.calledWith(6, 'q', 'a').should.equal(true);
+
+    dispatched.should.eql(dispatchAction);
+  });
+
+  it('should log event using intercom on OPEN_BOTTOM_SHEET_WITH_FAQ', function () {
+    let dispatched;
+    const dispatchAction = openBottomSheetWithFAQ(16);
+    const faq = {
+      'fields': [
+        {
+          'name': 'question',
+          'value': {
+            'blocks': [
+              {
+                'text': 'q'
+              }
+            ]
+          }
+        },
+        {
+          'name': 'answer',
+          'value': {
+            'blocks': [
+              {
+                'text': 'a'
+              }
+            ]
+          }
+        }
+      ],
+      'id': 16
+    };
+
+    IntercomLoggingMiddleware({
+      getState: () => ({ faqs: [faq] })
+    })(action => dispatched = action)(dispatchAction);
+    intercomUtils.trackClickedFaqItem.calledOnce.should.equal(true);
+    intercomUtils.trackClickedFaqItem.calledWith(16, 'q', 'a').should.equal(true);
 
     dispatched.should.eql(dispatchAction);
   });
