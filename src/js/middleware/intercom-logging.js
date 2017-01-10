571617975cb1b2d1b2bcd260f1018f44cac2ce27
ea-open-source <open-source@eastagile.com>
2017-01-10 11:01:24 +0700

    [r] 887b - Reactor IntercomLogging middleware
    https://github.com/EastAgile/CPDBv2_frontend/pull/123#discussion_r94934279

--- src/js/middleware/intercom-logging.js
+++ src/js/middleware/intercom-logging.js
@@ -5,26 +5,32 @@ import {
 import { EXPAND_FAQ } from 'actions/faq-page/index';
 import { trackClickedReportingItem, trackClickedFaqItem } from 'utils/intercom';
 import { getField, plainTextValueToString, multilineTextValueToArray } from 'utils/draft';
-import _ from 'lodash';
+import { find } from 'lodash';
 
-
-export default store => next => action => {
-  if (action.type === OPEN_BOTTOM_SHEET_WITH_REPORT) {
+const INTERCOM_TRACKING_MAP = {
+  [OPEN_BOTTOM_SHEET_WITH_REPORT]: (store, action) => {
     const report = action.payload;
     trackClickedReportingItem(report.id, report.title);
-  }
+  },
 
-  if (action.type === EXPAND_FAQ) {
+  [EXPAND_FAQ]: (store, action) => {
     const { id, question, answer } = action.payload;
     trackClickedFaqItem(id, question, answer);
-  }
+  },
 
-  if (action.type === OPEN_BOTTOM_SHEET_WITH_FAQ) {
+  [OPEN_BOTTOM_SHEET_WITH_FAQ]: (store, action) => {
     const faqs = store.getState().faqs;
-    const faq = _.find(faqs, ['id', action.payload]);
+    const faq = find(faqs, ['id', action.payload]);
     const question = plainTextValueToString(getField(faq.fields, 'question').value);
     const answer = multilineTextValueToArray(getField(faq.fields, 'answer').value).join('\n');
     trackClickedFaqItem(faq.id, question, answer);
+  }
+};
+
+
+export default store => next => action => {
+  if (INTERCOM_TRACKING_MAP.hasOwnProperty(action.type)) {
+    INTERCOM_TRACKING_MAP[action.type](store, action);
   }
 
   return next(action);
