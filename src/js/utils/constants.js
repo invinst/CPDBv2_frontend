9e7d472b3b66bbb1bdc2eec6458d2ab44b086eae
ea-open-source <open-source@eastagile.com>
2017-06-14 09:53:42 +0700

    [f] 491 - add finding colors for timeline page
    [#147165491] https://www.pivotaltracker.com/story/show/147165491

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -154,3 +154,14 @@ export const TimelineItemType = new Enum(['CR', 'UNIT', 'YEAR', 'JOINED']);
 
 export const MAPBOX_ACCESS_TOKEN =
   'pk.eyJ1IjoiaW52aXNpYmxlaW5zdGl0dXRlIiwiYSI6ImNpZ256bXRqMDAwMDBzeGtud3VoZGplNHMifQ.ky2VSGEYU5KritRMArHY-w';
+
+export const FINDING_COLORS = {
+  'Unfounded': 'rgba(23, 43, 58, 0.3)',
+  'Exonerated': 'rgba(98, 178, 140, 0.3)',
+  'Not Sustained': 'rgba(37, 138, 173, 0.3)',
+  'Sustained': 'rgba(255, 96, 0, 0.3)',
+  'No Cooperation': 'rgba(165, 180, 190, 0.3)',
+  'No Affidavit': 'rgba(112, 157, 192, 0.3)',
+  'Discharged': 'rgba(203, 203, 203, 0.3)',
+  'Unknown': 'rgba(152, 152, 152, 0.3)'
+};
