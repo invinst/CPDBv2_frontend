9db1ee5a9990748aac3c8b910a26784a4d0e7db8
ea-open-source <open-source@eastagile.com>
2017-06-30 09:55:02 +0700

    [r] use `hex` instead of `rgba`

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,5 +1,9 @@
 import Enum from 'enum';
 
+import {
+  darkSilverSandColor, skepticColor, jaggedIceColor, romanticColor, porcelainColor, botticelliColor,
+  galleryColor, lightAltoColor
+} from 'utils/styles';
 
 export const TOP = 'top';
 export const BOTTOM = 'bottom';
@@ -156,12 +160,12 @@ export const MAPBOX_ACCESS_TOKEN =
   'pk.eyJ1IjoiaW52aXNpYmxlaW5zdGl0dXRlIiwiYSI6ImNpZ256bXRqMDAwMDBzeGtud3VoZGplNHMifQ.ky2VSGEYU5KritRMArHY-w';
 
 export const FINDING_COLORS = {
-  'Unfounded': 'rgba(23, 43, 58, 0.3)',
-  'Exonerated': 'rgba(98, 178, 140, 0.3)',
-  'Not Sustained': 'rgba(37, 138, 173, 0.3)',
-  'Sustained': 'rgba(255, 96, 0, 0.3)',
-  'No Cooperation': 'rgba(165, 180, 190, 0.3)',
-  'No Affidavit': 'rgba(112, 157, 192, 0.3)',
-  'Discharged': 'rgba(203, 203, 203, 0.3)',
-  'Unknown': 'rgba(152, 152, 152, 0.3)'
+  'Unfounded': darkSilverSandColor,
+  'Exonerated': skepticColor,
+  'Not Sustained': jaggedIceColor,
+  'Sustained': romanticColor,
+  'No Cooperation': porcelainColor,
+  'No Affidavit': botticelliColor,
+  'Discharged': galleryColor,
+  'Unknown': lightAltoColor
 };
