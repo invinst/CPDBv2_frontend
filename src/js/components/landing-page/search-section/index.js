557b2b95e7cb78e20151e5e1368855759c289f50
ea-open-source <open-source@eastagile.com>
2017-09-13 14:32:21 +0700

    [b] 374 - Search alias edit mode not reachable on frontend
    Landing page’s Search section should link to `/edit/search/` if edit
    mode is currently on.
    
    [#151032374] https://www.pivotaltracker.com/story/show/151032374

--- src/js/components/landing-page/search-section/index.js
+++ src/js/components/landing-page/search-section/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { SEARCH_PATH } from 'utils/constants';
 import { searchSectionStyle, searchBoxStyle } from './search-section.style';
 
-const SearchSection = (props) => (
-  <div style={ searchSectionStyle }>
-    <Link style={ searchBoxStyle } to={ '/' + SEARCH_PATH }>Search</Link>
-  </div>
-);
+const SearchSection = (props, context) => {
+  const editPath = context.editModeOn ? '/edit' : '';
+  const to = `${editPath}/${SEARCH_PATH}`;
+
+  return (
+    <div style={ searchSectionStyle }>
+      <Link style={ searchBoxStyle } to={ to }>Search</Link>
+    </div>
+  );
+};
+
+SearchSection.contextTypes = {
+  editModeOn: PropTypes.bool
+};
 
 export default SearchSection;
