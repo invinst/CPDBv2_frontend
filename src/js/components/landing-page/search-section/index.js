924c05659c59b704affcac9967fefc13425944fd
ea-open-source <open-source@eastagile.com>
2017-09-19 09:21:25 +0700

    [c] Make use of editMode() helper

--- src/js/components/landing-page/search-section/index.js
+++ src/js/components/landing-page/search-section/index.js
@@ -1,11 +1,11 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { SEARCH_PATH } from 'utils/constants';
+import { editMode } from 'utils/edit-path';
 import { searchSectionStyle, searchBoxStyle } from './search-section.style';
 
 const SearchSection = (props, context) => {
-  const editPath = context.editModeOn ? '/edit' : '';
-  const to = `${editPath}/${SEARCH_PATH}`;
+  const to = context.editModeOn ? editMode(SEARCH_PATH) : `/${SEARCH_PATH}`;
 
   return (
     <div style={ searchSectionStyle }>
