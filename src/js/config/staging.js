881bb5b9a913756797762b9d51c0fb79c4581801
ea-open-source <open-source@eastagile.com>
2017-12-22 09:47:55 +0700

    [f] 122 - Update v1 url for staging env
    [#153621122] https://www.pivotaltracker.com/story/show/153621122

--- src/js/config/staging.js
+++ src/js/config/staging.js
@@ -1,6 +1,6 @@
 export default {
   appEnv: 'staging',
   heatMapContainer: 'https://cpdbv23689.blob.core.windows.net/heatmap/',
-  v1Url: 'https://beta.cpdb.co'
+  v1Url: 'https://staging.cpdb.co'
 };
 
