9a6136254f5124cff171e69b0ec3c55be0a4d2fd
ea-open-source <open-source@eastagile.com>
2018-01-02 16:06:42 +0700

    [f] 865 - Fix live tests
    [#153953865] https://www.pivotaltracker.com/story/show/153953865

--- src/js/mock-api/landing-page/suggestions.js
+++ src/js/mock-api/landing-page/suggestions.js
@@ -1,25 +1,23 @@
 import { RawOfficerSuggestion, RawNeighborhoodSuggestion } from 'utils/test/factories/suggestion';
 
 
-export default {
+export const groupedSuggestions = {
   'default': {
-    'OFFICER': [RawOfficerSuggestion.build({
-      id: '1'
-    }, {
-      resultText: 'Bernadette Kelly',
-      to: '/officer/1/',
-      allegationCount: 10,
-      sustainedCount: 2,
-      birthYear: 1972,
-      race: 'White',
-      sex: 'Male'
-    }), RawOfficerSuggestion.build({
-      id: '2'
-    }), ...RawOfficerSuggestion.buildList(8)],
+    'OFFICER': [
+      RawOfficerSuggestion.build({ id: '1' }, {
+        resultText: 'Bernadette Kelly',
+        to: '/officer/1/',
+        allegationCount: 10,
+        sustainedCount: 2,
+        birthYear: 1972,
+        race: 'White',
+        sex: 'Male'
+      }),
+      RawOfficerSuggestion.build({ id: '2' }),
+      ...RawOfficerSuggestion.buildList(8)
+    ],
     'CO-ACCUSED': [
-      RawOfficerSuggestion.build({
-        id: '1'
-      }, {
+      RawOfficerSuggestion.build({ id: '1' }, {
         resultText: 'Bernadette Kelly',
         to: '/officer/1/',
         allegationCount: 10,
@@ -37,15 +35,28 @@ export default {
     ]
   },
   'noresult': {},
-  'OFFICER': {
-    'OFFICER': [
-      RawOfficerSuggestion.build({}, { resultText: 'Bernadette Kelly' }),
-      RawOfficerSuggestion.build({}, { resultText: 'Charles Kelly' })
-    ]
-  },
   'foo': {
     'OFFICER': [
       RawOfficerSuggestion.build({}, { resultText: 'Laurence Lanners', to: '/officer/5678/' })
     ]
+  }
+};
+
+export const singleGroupSuggestions = {
+  'default': {
+    count: 30,
+    previous: null,
+    next: 'http://my/api/?contentType=OFFICER&offset=20',
+    results: [
+      RawOfficerSuggestion.build({}, { resultText: 'Bernadette Kelly' }),
+      RawOfficerSuggestion.build({}, { resultText: 'Charles Kelly' }),
+      ...RawOfficerSuggestion.buildList(18)
+    ]
+  },
+  offset20: {
+    count: 30,
+    previous: null,
+    next: null,
+    results: RawOfficerSuggestion.buildList(10)
   }
 };
