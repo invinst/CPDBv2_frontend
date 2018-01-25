dad264e7a65bcfce2834894cda1ae46059e8c468
ea-open-source <open-source@eastagile.com>
2018-01-25 09:58:13 +0700

    [f] 468 - Remove PropsRerender and redundant code
    [#154319468] https://www.pivotaltracker.com/story/show/154319468

--- test/components/common/higher-order/props-rerender.js
+++
@@ -1,8 +0,0 @@
-import PropsRerender from 'components/common/higher-order/props-rerender';
-
-
-describe('PropsRerender component', function () {
-  it('should render sub-component', function () {
-    PropsRerender.should.renderSubComponent();
-  });
-});
