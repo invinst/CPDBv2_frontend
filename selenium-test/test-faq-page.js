95f5f631045796d7202a55be5eab25a05f7eb23d
ea-open-source <open-source@eastagile.com>
2018-02-28 09:23:36 +0700

    [c] 538 - Fixed faq editable add button live test to be more reliable
    [#155541538](https://www.pivotaltracker.com/story/show/155541538)

--- selenium-test/test-faq-page.js
+++ selenium-test/test-faq-page.js
@@ -44,15 +44,13 @@ describe('FAQ page', function () {
     });
 
     it('should open empty editable bottom sheet when click on add button', function () {
-      this.retries(3);
-
       faqPage.faqSection.addButton.click();
 
       faqPage.bottomSheet.faq.question.waitForVisible();
       faqPage.bottomSheet.faq.answer.waitForVisible();
 
-      faqPage.isRichTextEditorEmpty(faqPage.bottomSheet.faq.question).should.be.true();
-      faqPage.isRichTextEditorEmpty(faqPage.bottomSheet.faq.answer).should.be.true();
+      faqPage.bottomSheet.faq.questionPlaceHolder.getText().should.eql('Question');
+      faqPage.bottomSheet.faq.answerPlaceHolder.getText().should.eql('Answer');
     });
 
     it('should open edittable bottom sheet when click on question', function () {
