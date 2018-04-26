0bad302ee5b3a0814c88dbd6651d0f571c5c4cdc
ea-open-source <open-source@eastagile.com>
2018-04-26 15:34:11 +0700

    [f] 831 - add marker asset and retina resolution of map image
    [##156792831] https://www.pivotaltracker.com/story/show/#156792831

--- src/js/utils/mapbox.js
+++ src/js/utils/mapbox.js
@@ -3,6 +3,7 @@ import 'mapbox.js';
 import { MAPBOX_ACCESS_TOKEN } from 'utils/constants';
 
 const MAPBOX_API_URL = 'https://api.mapbox.com/styles/v1/mapbox';
+const MARKER_URL = 'https://cpdbv21777.blob.core.windows.net/assets/map-marker.png';
 
 global.L.mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
 
@@ -14,7 +15,7 @@ export const getComplaintMapUrl = (lat, lon, width, height) => [
   MAPBOX_API_URL,
   'streets-v10',
   'static',
-  `pin-s+ff0000(${lon},${lat})`,
+  `url-${encodeURIComponent(MARKER_URL)}(${lon},${lat})`,
   `${lon},${lat},12,0,0`,
-  `${width}x${height}?access_token=${MAPBOX_ACCESS_TOKEN}`,
+  `${width}x${height}@2x?access_token=${MAPBOX_ACCESS_TOKEN}`,
 ].join('/');
