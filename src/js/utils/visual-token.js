73b6ad75acdc12a9c05527d09eb77470be8c3b62
ea-open-source <open-source@eastagile.com>
2018-05-21 14:51:29 +0700

    [b] 965 - switch visual token order, internal allegations and civilian allegations
    [#157721965] https://www.pivotaltracker.com/story/show/157721965

--- src/js/utils/visual-token.js
+++ src/js/utils/visual-token.js
@@ -14,16 +14,16 @@ export const scalePercentile = (val) => {
   return val !== 0 ? parseInt((val - 0.0001) / 20) + 1: 0;
 };
 
-export const getVisualTokenOIGBackground = (internalPercentile, civilPercentile, useOfForcePercentile) => {
+export const getVisualTokenOIGBackground = (civilPercentile, internalPercentile, useOfForcePercentile) => {
   const { LIGHT_COLOR, DARK_COLOR, COLOR_TEXT_LIGHT_SCHEME } = constants.OIG_VISUAL_TOKEN_COLOR_SCHEME_TEXT;
   const key = [
-    scalePercentile(internalPercentile),
+    scalePercentile(civilPercentile),
     scalePercentile(useOfForcePercentile)
   ].join('');
   return {
     backgroundColor: key !== '00' ?
       constants.OIG_VISUAL_TOKEN_COLOR_SCHEME[key] :
-      constants.OIG_EXTRA_BLUE_COLOR_SCHEME[scalePercentile(civilPercentile)],
+      constants.OIG_EXTRA_BLUE_COLOR_SCHEME[scalePercentile(internalPercentile)],
     textColor: COLOR_TEXT_LIGHT_SCHEME.indexOf(key) === -1 ? DARK_COLOR : LIGHT_COLOR,
   };
 };
