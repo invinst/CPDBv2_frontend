b1a66c4cd6180a3b0ea0f92aa11acc756e474c22
ea-open-source <open-source@eastagile.com>
2018-06-06 11:45:42 +0700

    [f] 219 - mix finding + outcome text on accused card in CR detail page
    [#157583219] https://www.pivotaltracker.com/story/show/157583219

---
+++ test/selectors/cr-page/finding-outcome-mix.js
@@ -0,0 +1,14 @@
+import { getFindingOutcomeMix } from 'selectors/cr-page/finding-outcome-mix';
+
+
+describe('outcome tranformation', function () {
+  describe('getFindingOutcomeMix', function () {
+    it('should return correct final decision text', function () {
+      getFindingOutcomeMix('Unknown', '10 Day Suspension').should.eql(['Suspended 10 days']);
+      getFindingOutcomeMix('Unknown', 'Administrative Termination').should.eql(['Administrative Termination']);
+      getFindingOutcomeMix('Unfounded', 'Unknown').should.eql(['Unfounded']);
+      getFindingOutcomeMix('Exonerated', 'No Action Taken').should.eql(['Exonerated']);
+      getFindingOutcomeMix('Unfounded', 'Violation Noted').should.eql(['Unfounded', 'Violation Noted']);
+    });
+  });
+});
