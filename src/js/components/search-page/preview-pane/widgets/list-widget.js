f35215faf2c2b79e73419a50166425ce53a0ae51
ea-open-source <open-source@eastagile.com>
2018-06-07 10:10:33 +0700

    [f] 048 - Preview panes radar chart thumbnail
    [#157926048] https://www.pivotaltracker.com/story/show/157926048

--- src/js/components/search-page/preview-pane/widgets/list-widget.js
+++ src/js/components/search-page/preview-pane/widgets/list-widget.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { map } from 'lodash';
 import pluralize from 'pluralize';
 
+import StaticRadarChart from 'components/common/radar-chart';
 import {
   containerStyle,
   headerStyle,
@@ -9,32 +9,44 @@ import {
   listItemFirstStyle,
   listItemStyle,
   listStyle,
-  itemNameStyle
+  itemNameStyle,
+  chartWrapperStyle
 } from './list-widget.style';
 
 
 export default class ListWidget extends Component {
   render() {
     const { items, title, typeName, showAvatar } = this.props;
-    return !!(items && items.length > 0) && (
-      <div className='test--list-widget' style={ containerStyle }>
-        <h5 style={ headerStyle }>{ title }</h5>
-        <ul style={ listStyle }>
-          { map(items, (item, index) => (
-            <li key={ item.id } style={ listItemStyle(index === items.length - 1) }>
-              { (showAvatar) && (
-                <div style={ listItemFirstStyle }>
-                  <img src={ item.image || 'http://via.placeholder.com/32x32' }/>
+    return (
+      items && items.length > 0 ? (
+        <div className='test--list-widget' style={ containerStyle }>
+          <h5 style={ headerStyle }>{ title }</h5>
+          <ul style={ listStyle }>
+            { items.map((item, index) => (
+              <li key={ item.id } style={ listItemStyle(index === items.length - 1) }>
+                { (showAvatar) && (
+                  <div style={ listItemFirstStyle }>
+                    <div style={ chartWrapperStyle } className='test--preview-pane-thumbnail'>
+                      <StaticRadarChart
+                        width={ 38 }
+                        height={ 38 }
+                        radius={ 18 }
+                        hideAxisText={ true }
+                        data={ item.radarAxes }
+                        key={ index }
+                        { ...item.radarColor } />
+                    </div>
+                  </div>
+                ) }
+                <div>
+                  <p style={ itemNameStyle }>{ item.name }</p>
+                  <p style={ itemCountStyle }>{ pluralize(typeName, item.count, true) }</p>
                 </div>
-              ) }
-              <div>
-                <p style={ itemNameStyle }>{ item.name }</p>
-                <p style={ itemCountStyle }>{ pluralize(typeName, item.count, true) }</p>
-              </div>
-            </li>
-          )) }
-        </ul>
-      </div>
+              </li>
+            )) }
+          </ul>
+        </div>
+      ) : null
     );
   }
 }
@@ -46,13 +58,13 @@ ListWidget.defaultProps = {
 
 ListWidget.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    name: PropTypes.string.isRequired,
-    count: PropTypes.number.isRequired,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    name: PropTypes.string,
+    count: PropTypes.number,
     image: PropTypes.string.optional,
     url: PropTypes.string.optional
-  })).isRequired,
+  })),
   title: PropTypes.string,
-  typeName: PropTypes.string.isRequired,
+  typeName: PropTypes.string,
   showAvatar: PropTypes.bool,
 };
