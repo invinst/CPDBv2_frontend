f9096e9b3b3bb9a74f626c4da4665bfb201ee4b8
ea-open-source <open-source@eastagile.com>
2018-06-07 17:25:32 +0700

    [f] 894 - CR page: Unfocus distance dropdown when click away
    [#157924894](https://www.pivotaltracker.com/story/show/157924894)

--- src/js/components/cr-page/related-complaints/dropdown.js
+++ src/js/components/cr-page/related-complaints/dropdown.js
@@ -11,6 +11,10 @@ export class Dropdown extends Component {
   constructor(props) {
     super(props);
 
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleOptionSelect = this.handleOptionSelect.bind(this);
+    this.handleOnBlur = this.handleOnBlur.bind(this);
+
     this.state = {
       expanded: false
     };
@@ -29,20 +33,30 @@ export class Dropdown extends Component {
     });
   }
 
+  handleOnBlur() {
+    this.setState({ expanded: false });
+  }
+
   render() {
     const { options, value, hovering } = this.props;
     const { expanded } = this.state;
 
     return (
-      <div style={ wrapperStyle }>
+      <div
+        style={ wrapperStyle }
+        onBlur={ this.handleOnBlur }
+        tabIndex='-1'
+        className='dropdown'
+      >
         <div
           className='test--related-complaint-dropdown'
           style={ displayOptionStyle }
-          onClick={ this.handleToggle.bind(this) }>
+          onClick={ this.handleToggle }
+        >
           <span style={ displayValueStyle(hovering) }>{ options[value] }</span>
           <span style={ arrowStyle(expanded) } />
         </div>
-        <div style={ optionsStyle(expanded) }>
+        <div style={ optionsStyle(expanded) } >
           {
             entries(options).map((option, index) => (
               value !== option[0] ? (
@@ -50,7 +64,7 @@ export class Dropdown extends Component {
                   key={ index }
                   className='test--related-complaint-dropdown-item'
                   style={ optionItemStyle }
-                  onClick={ this.handleOptionSelect.bind(this, option[0]) }>
+                  onClick={ () => this.handleOptionSelect(option[0]) }>
                   { option[1] }
                 </div>
               )
