90779a2abda70d08cb5a227c15eb7d82a676e76f
ea-open-source <open-source@eastagile.com>
2018-07-10 16:39:28 +0700

    667 - Reset to Timeline tab when entering new officer page
    [#158242667] https://www.pivotaltracker.com/story/show/158242667

--- src/js/reducers/officer-page/currentTab.js
+++ src/js/reducers/officer-page/currentTab.js
@@ -1,10 +1,13 @@
 import { handleActions } from 'redux-actions';
 
-import { CHANGE_OFFICER_TAB, OFFICER_PAGE_TAB_NAMES } from 'utils/constants';
+import { CHANGE_OFFICER_TAB, OFFICER_PAGE_TAB_NAMES, CHANGE_OFFICER_ID } from 'utils/constants';
 
 
 export default handleActions({
   [CHANGE_OFFICER_TAB]: (state, action) => {
     return action.payload;
+  },
+  [CHANGE_OFFICER_ID]: (state, action) => {
+    return OFFICER_PAGE_TAB_NAMES.TIMELINE;
   },
 }, OFFICER_PAGE_TAB_NAMES.TIMELINE);
