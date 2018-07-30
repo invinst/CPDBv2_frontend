4fb2c4848494d3f0c3e374fafb139758b0e4e855
ea-open-source <open-source@eastagile.com>
2018-07-30 12:10:09 +0700

    [f] 894 - Pressing enter on Link term open new tab with v1.2 data tool
    [#158920894] https://www.pivotaltracker.com/story/show/158920894

--- src/js/utils/navigate-to-search-item.js
+++ src/js/utils/navigate-to-search-item.js
@@ -1,11 +1,14 @@
 import { browserHistory } from 'react-router';
 
+import { trackOutboundLink } from 'utils/tracking';
+
+
 export const navigateToSearchItem = (item, beforeNavigate) => {
   beforeNavigate(item);
   if (item.to) {
     browserHistory.push(item.to);
   } else {
     /* istanbul ignore next */
-    window.location.assign(item.url);
+    trackOutboundLink(item.url, '_blank');
   }
 };
