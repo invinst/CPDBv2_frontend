39df9fb8898aec72154147e8a4fc25464b14b984
ea-open-source <open-source@eastagile.com>
2018-08-03 13:45:38 +0700

    [f] 924 - remove redundant comment line
    [#159388924] https://www.pivotaltracker.com/story/show/159388924

--- src/js/components/landing-page/heat-map/summary-panel/summary-panel.style.js
+++ src/js/components/landing-page/heat-map/summary-panel/summary-panel.style.js
@@ -4,7 +4,6 @@ import { sanFranciscoTextFamily } from 'utils/styles';
 export const panelStyle = top => ({
   position: 'relative',
   top: `${top}px`,
-  // left: 'calc(50% + 130px)',
   width: '320px',
   minHeight: '112px',
   background: 'transparent',
