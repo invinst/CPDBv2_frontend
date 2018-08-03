be9e51a5d0d0f209cd9d981bbdf2ef5d571dbb30
ea-open-source <open-source@eastagile.com>
2018-08-03 10:28:27 +0700

    [f] 884 - Correct year range on heat map header
    [#158132884](https://www.pivotaltracker.com/story/show/158132884)

--- test/components/landing-page/heat-map/summary-panel/city-summary.js
+++ test/components/landing-page/heat-map/summary-panel/city-summary.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { findDOMNode } from 'react-dom';
 import { each } from 'lodash';
-import { renderIntoDocument } from 'react-addons-test-utils';
+import { renderIntoDocument, findRenderedDOMComponentWithClass } from 'react-addons-test-utils';
 
 import { unmountComponentSuppressError } from 'utils/test';
 import CitySummary from 'components/landing-page/heat-map/summary-panel/city-summary';
@@ -30,10 +30,17 @@ describe('CitySummary component', function () {
       }
     ];
     const citySummary = {
+      startYear: 1999,
+      endYear: 2017,
       mostCommonComplaints
     };
 
     instance = renderIntoDocument(<CitySummary citySummary={ citySummary }/>);
+
+    findRenderedDOMComponentWithClass(
+      instance, 'test--city-summary-header'
+    ).textContent.should.equal('CHICAGO 1999 - 2017');
+
     const element = findDOMNode(instance);
     each(mostCommonComplaints, ({ name, count }) => {
       element.textContent.should.containEql(name);
