beb18e16f3d2224d9f892645a7050f979500ce75
ea-open-source <open-source@eastagile.com>
2018-09-07 17:19:01 +0700

    [f] 034 - Change WrapperLink into class component
    [#160067034] https://www.pivotaltracker.com/story/show/160067034

--- test/components/search-page/preview-pane/widgets/call-to-action-widget.js
+++ test/components/search-page/preview-pane/widgets/call-to-action-widget.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  renderIntoDocument,
-  findRenderedComponentWithType,
-} from 'react-addons-test-utils';
-import { Link } from 'react-router';
 
-import OutboundLink from 'components/common/outbound-link';
 import CallToActionWidget from 'components/search-page/preview-pane/widgets/call-to-action-widget';
 import { unmountComponentSuppressError } from 'utils/test';
 
@@ -19,19 +13,5 @@ describe('CallToActionWidget component', () => {
 
   it('should be renderable', () => {
     CallToActionWidget.should.be.renderable();
-  });
-
-  it('should contain a Link component when it has `to` property', () => {
-    instance = renderIntoDocument(
-      <CallToActionWidget to='officer/1/'/>
-    );
-    findRenderedComponentWithType(instance, Link).props.to.should.eql('officer/1/');
-  });
-
-  it('should contain a Link component when it has `to` property', () => {
-    instance = renderIntoDocument(
-      <CallToActionWidget url='something.co'/>
-    );
-    findRenderedComponentWithType(instance, OutboundLink).props.href.should.eql('something.co');
   });
 });
