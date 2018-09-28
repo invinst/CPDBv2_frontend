1eacf9c122fd60560283e9bc8fb50d32a08ffa4f
ea-open-source <open-source@eastagile.com>
2018-09-28 12:18:11 +0700

    [f] 327 - Update Request Documents button on CR's with no attachments to make it more discoverable
    [#159388327] https://www.pivotaltracker.com/story/show/159388327

---
+++ src/js/components/cr-page/attachments/headers/attachment-header.js
@@ -0,0 +1,36 @@
+import React, { PropTypes, Component } from 'react';
+
+import RequestDocumentButton from 'components/common/request-document-button';
+import { wrapperStyle, titleStyle, subTitleStyle, requestButtonStyle, messageStyle } from './attachment-header.style';
+
+
+export default class AttachmentHeader extends Component {
+  render() {
+    const { openRequestDocumentModal, alreadyRequested } = this.props;
+
+    return (
+      <div style={ wrapperStyle }>
+        <div style={ messageStyle }>
+          <span style={ titleStyle }>ATTACHMENTS</span>
+          <span style={ subTitleStyle }>MAY CONTAIN GRAPHIC CONTENT</span>
+        </div>
+        <div style={ requestButtonStyle }>
+          <RequestDocumentButton
+            alreadyRequested={ alreadyRequested }
+            openRequestDocumentModal={ openRequestDocumentModal }
+            hasData={ true }
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+AttachmentHeader.defaultProps = {
+  items: []
+};
+
+AttachmentHeader.propTypes = {
+  openRequestDocumentModal: PropTypes.func,
+  alreadyRequested: PropTypes.bool,
+};
