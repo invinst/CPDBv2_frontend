56a66558524097a662bd3f24943504e3f167a3ba
ea-open-source <open-source@eastagile.com>
2018-10-15 14:32:28 +0700

    [f] 255 - Fix failing test:
    [#160784255] https://www.pivotaltracker.com/story/show/160784255

--- test/utils/static-assets.js
+++ test/utils/static-assets.js
@@ -1,14 +1,10 @@
 import { imgUrl } from 'utils/static-assets';
-import config from 'config';
 
 
 describe('Static assets module', function () {
   describe('imgUrl function', function () {
     it('should prepend config static base url to path', function () {
-      const oldStaticBase = config.staticBase;
-      config.staticBase = 'http://cdn';
-      imgUrl('abc').should.equal('http://cdn/img/abc');
-      config.staticBase = oldStaticBase;
+      imgUrl('abc').should.equal('/img/abc');
     });
   });
 });
