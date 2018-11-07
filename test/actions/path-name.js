fa5de4611f9a6fedf2eec655a54783d98ae98928
ea-open-source <open-source@eastagile.com>
2018-11-07 16:47:09 +0700

    [f] 535 - Add unit tests
    [#161675535] https://www.pivotaltracker.com/story/show/161675535

---
+++ test/actions/path-name.js
@@ -0,0 +1,14 @@
+import { updatePathName } from 'actions/path-name';
+import { UPDATE_PATH_NAME } from 'utils/constants';
+
+
+describe('Path name actions', function () {
+  describe('updatePathName', function () {
+    it('should return correct payload', function () {
+      updatePathName('/some/path').should.eql({
+        type: UPDATE_PATH_NAME,
+        payload: '/some/path'
+      });
+    });
+  });
+});
