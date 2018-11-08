73dfced557e04506dff6577e692e67f273ea3880
ea-open-source <open-source@eastagile.com>
2018-11-08 09:33:13 +0700

    Add more tests

---
+++ test/reducers/embed/officers.js
@@ -0,0 +1,16 @@
+import officersReducer from 'reducers/embed/officers';
+import * as constants from 'utils/constants';
+
+
+describe('officersReducer', function () {
+  it('should return initial state', function () {
+    officersReducer(undefined, {}).should.eql([]);
+  });
+
+  it('should handle EMBED_OFFICERS_REQUEST_SUCCESS', function () {
+    officersReducer([], {
+      type: constants.EMBED_OFFICERS_REQUEST_SUCCESS,
+      payload: [1, 2, 3]
+    }).should.eql([1, 2, 3]);
+  });
+});
