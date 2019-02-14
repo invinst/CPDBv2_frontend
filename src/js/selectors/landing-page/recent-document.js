ba66fe00f9bb4b82027b3ec2c85426f418f40b56
ea-open-source <open-source@eastagile.com>
2019-02-14 14:10:30 +0700

    [f] 481 - Remove shuffle behavior on front page’s recent documents row
    [#163518481] (https://www.pivotaltracker.com/story/show/163518481)

--- src/js/selectors/landing-page/recent-document.js
+++ src/js/selectors/landing-page/recent-document.js
@@ -1,5 +1,4 @@
 import { createSelector } from 'reselect';
-import { shuffle } from 'lodash';
 
 
 export const getCarouselDocumentHeaderEditModeOn = state => state.landingPage.recentDocument.headerEditModeOn;
@@ -20,9 +19,5 @@ export const hasCards = createSelector(
 
 export const cardsSelector = createSelector(
   [getCards],
-  cards => {
-    const upperHalf = shuffle(cards.slice(0, 12));
-    const lowerHalf = shuffle(cards.slice(12));
-    return upperHalf.concat(lowerHalf).map(cardTransform);
-  }
+  cards => cards.map(cardTransform)
 );
