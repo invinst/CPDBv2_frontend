f0e592655cb79cb9ed9fb9f950c2ed61755fd20a
ea-open-source <open-source@eastagile.com>
2019-03-04 13:22:21 +0700

    Add more unit test

--- test/components/crawlers-page/crawler-table.js
+++ test/components/crawlers-page/crawler-table.js
@@ -62,4 +62,37 @@ describe('CrawlerTable component', function () {
     infiniteScroll.props.children.should.have.length(2);
     scryRenderedComponentsWithType(instance, CrawlerRow).should.have.length(2);
   });
+
+  it('should load more on scroll to bottom', function () {
+    const rows = [{
+      id: 109,
+      crawlerName: 'SUMMARY_REPORTS_COPA',
+      numDocuments: 5,
+      numNewDocuments: 1,
+      recentRunAt: '2019-02-20'
+    }, {
+      id: 110,
+      crawlerName: 'SUMMARY_REPORTS_COPA',
+      numDocuments: 7,
+      numNewDocuments: 2,
+      recentRunAt: '2019-02-20'
+    }];
+    const nextParams = {
+      limit: 1,
+      offset: 1
+    };
+
+    const requestCrawlersStub = stub().returns({ catch: stub() });
+
+    instance = renderIntoDocument(
+      <CrawlerTable
+        rows={ rows }
+        hasMore={ true }
+        nextParams={ nextParams }
+        requestCrawlers={ requestCrawlersStub }/>
+    );
+    findRenderedComponentWithType(instance, InfiniteScroll).props.loadMore();
+    requestCrawlersStub.calledWith({ ...nextParams }).should.be.true();
+  });
+
 });
