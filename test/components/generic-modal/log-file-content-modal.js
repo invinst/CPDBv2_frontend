85152396ef9370b4ef5df24b850dc8b6f0ba94b6
ea-open-source <open-source@eastagile.com>
2019-03-22 17:58:30 +0700

    [f] 957 - Add more unit test
    [#164487957] (https://www.pivotaltracker.com/story/show/164487957)

--- test/components/generic-modal/log-file-content-modal.js
+++ test/components/generic-modal/log-file-content-modal.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { stub } from 'sinon';
 import {
   renderIntoDocument,
   findRenderedDOMComponentWithClass,
+  Simulate,
 } from 'react-addons-test-utils';
 
 import LogFileModalContent from 'components/generic-modal/log-file-modal-content';
@@ -23,5 +25,22 @@ describe('LogFileModalContent component', function () {
     findRenderedDOMComponentWithClass(instance, 'embed-content').getAttribute('src').should.be.eql(
       'https://lvh.me/log'
     );
+  });
+
+  it('should close model when click on close button', function () {
+    const closeModalStub = stub();
+    const crawler = {
+      crawlerName: 'PORTAL_COPA',
+      logUrl: 'https://lvh.me/log',
+      recentRunAt: '2019-03-04'
+    };
+
+    let instance = renderIntoDocument(
+      <LogFileModalContent crawler={ crawler } closeModal={ closeModalStub } />
+    );
+
+    const closeButton = findRenderedDOMComponentWithClass(instance, 'log-file-close-button');
+    Simulate.click(closeButton);
+    closeModalStub.called.should.be.true();
   });
 });
