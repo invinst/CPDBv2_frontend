bac14b7a4741e41733ba69d1f1eedad8c939adf1
ea-open-source <open-source@eastagile.com>
2019-06-13 14:05:59 +0700

    [f] 059 - Refactor code and add test
    [#165486059] https://www.pivotaltracker.com/story/show/165486059

--- test/selectors/pinboard-page/items.js
+++ test/selectors/pinboard-page/items.js
@@ -10,8 +10,8 @@ import {
 
 describe('Pinboard items selectors', function () {
   describe('pinnedOfficersSelector selector', function () {
-    it('should return transformed officers', function () {
-      const state = {
+    it.only('should return transformed officers', function () {
+      pinnedOfficersSelector({
         pinboardPage: {
           officerItems: [{
             id: 1,
@@ -33,9 +33,63 @@ describe('Pinboard items selectors', function () {
             }
           }],
         }
-      };
+      }).should.eql([{
+        id: '1',
+        type: 'OFFICER',
+        isPinned: true,
+        birthYear: 1975,
+        complaintCount: 0,
+        complaintPercentile: 99.345,
+        fullName: 'Daryl Mack',
+        gender: 'male',
+        officerId: 1,
+        percentile: {
+          items: [
+            {
+              axis: 'Use of Force Reports',
+              value: 12,
+            },
+            {
+              axis: 'Officer Allegations',
+              value: 99.784,
+            },
+            {
+              axis: 'Civilian Allegations',
+              value: 98.4344,
+            },
+          ],
+          textColor: '#231F20',
+          visualTokenBackground: '#ff4f13',
+          year: 2016,
+        },
+        race: 'white',
+        rank: 'Police Officer',
+        sustainedCount: 0,
+      }]);
 
-      pinnedOfficersSelector(state).should.eql([{
+      pinnedOfficersSelector({
+        pinboardPage: {
+          officerItems: [{
+            id: 1,
+            'full_name': 'Daryl Mack',
+            'complaint_count': 0,
+            'sustained_count': 0,
+            'birth_year': 1975,
+            'complaint_percentile': 99.3450,
+            race: 'White',
+            gender: 'Male',
+            rank: 'Police Officer',
+            percentiles: [{
+              'percentile_trr': '12.0000',
+              'percentile_allegation': '99.3450',
+              'percentile_allegation_civilian': '98.4344',
+              'percentile_allegation_internal': '99.7840',
+              year: 2016,
+              id: 1,
+            }]
+          }],
+        }
+      }).should.eql([{
         id: '1',
         type: 'OFFICER',
         isPinned: true,
