78232f4f420e06d835fcf0813843777781cf438f
ea-open-source <open-source@eastagile.com>
2019-07-03 17:13:30 +0700

    [f] 144b - Link beta with v1 production
    [#166895630] https://www.pivotaltracker.com/story/show/166895630

--- src/js/config/beta.js
+++ src/js/config/beta.js
@@ -1,6 +1,6 @@
 export default {
   appEnv: 'beta',
-  v1Url: 'https://staging.cpdb.co',
+  v1Url: 'https://data.cpdp.co',
   twitterBotName: 'PbotCpd',
   heatMapContainer: 'https://cpdbv2beta.blob.core.windows.net/heatmap/',
   apiDomain: `${global.location.protocol}//betaapi.cpdp.co`
