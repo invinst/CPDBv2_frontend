2f232d3d61eeea518bc02daecd8fddc9237ff111
ea-open-source <open-source@eastagile.com>
2019-07-15 17:53:16 +0700

    [f] 956a - Support mainElementSelector in Section
    [#167099614] https://www.pivotaltracker.com/story/show/167099614

--- selenium-test/page-objects/sections/section.js
+++ selenium-test/page-objects/sections/section.js
@@ -4,9 +4,13 @@ const { keys, forEach } = require('lodash');
 
 
 export default class Section {
-  prepareElementGetters(elements) {
+  // If you want to use mainElementSelector, make sure all the child selectors are in XPath format
+  prepareElementGetters(elements={}) {
+    if (this.mainElementSelector) {
+      elements.mainElement = '';
+    }
     forEach(keys(elements), key => {
-      const val = elements[key];
+      const val = this.mainElementSelector ? `${this.mainElementSelector}${elements[key]}` : elements[key];
       Object.defineProperty(this, key, { get: function () {
         const element = browser.element(val);
         element.count = browser.elements(val).value.length;
