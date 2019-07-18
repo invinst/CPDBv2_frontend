1a00c00be229fe374dbc79a079fa8d35fc8bd95f
ea-open-source <open-source@eastagile.com>
2019-07-18 16:00:11 +0700

    [f] 956a - Fix unclickable left half
    [#167099614] https://www.pivotaltracker.com/story/show/167099614

--- selenium-test/page-objects/sections/section.js
+++ selenium-test/page-objects/sections/section.js
@@ -4,39 +4,41 @@ const { keys, forEach, has } = require('lodash');
 
 
 export default class Section {
-  constructor(parentSelector='', mainElementSelector='') {
+  constructor(parentSelector = '', mainElementSelector = '') {
     this.parentSelector = parentSelector;
-    this.mainElementSelector = `${this.parentSelector}${mainElementSelector}`;
+    this.mainElementSelector = `${ this.parentSelector }${ mainElementSelector }`;
   }
 
   _buildSelector(rawSelector) {
     if (rawSelector.startsWith('(')) {
-      return `(${this.mainElementSelector}${ rawSelector.substring(1) }`;
+      return `(${ this.mainElementSelector }${ rawSelector.substring(1) }`;
     }
-    return `${this.mainElementSelector}${rawSelector}`;
+    return `${ this.mainElementSelector }${ rawSelector }`;
   }
 
   // If you want to use mainElement or parentSelector,
   // make sure all the child selectors are in XPath format
-  prepareElementGetters(elements={}) {
+  prepareElementGetters(elements = {}) {
     elements.mainElement = '';
     forEach(keys(elements), key => {
-      if (typeof elements[key] === 'function') {
-        Object.defineProperty(this, key, {
-          value: new elements[key](this.mainElementSelector),
-          writable: false
-        });
-      } else if (typeof elements[key] === 'string') {
-        const selector = this._buildSelector(elements[key]);
-        if (selector && !has(this, key)) {
+      if (!this.hasOwnProperty(key)) {
+        if (typeof elements[key] === 'function') {
           Object.defineProperty(this, key, {
-            get: function () {
-              const element = browser.element(selector);
-              element.count = browser.elements(selector).value.length;
-              element.selector = selector;
-              return element;
-            }
+            value: new elements[key](this.mainElementSelector),
+            writable: false,
           });
+        } else if (typeof elements[key] === 'string') {
+          const selector = this._buildSelector(elements[key]);
+          if (selector && !has(this, key)) {
+            Object.defineProperty(this, key, {
+              get: function () {
+                const element = browser.element(selector);
+                element.count = browser.elements(selector).value.length;
+                element.selector = selector;
+                return element;
+              },
+            });
+          }
         }
       }
     });
