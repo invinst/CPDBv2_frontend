c11d3ae7e8da356c5b7323f6bc3f58b1fa7c31be
ea-open-source <open-source@eastagile.com>
2019-10-04 17:06:19 +0700

    [f] 311 - Add pinning function live tests
    [#168798311] https://www.pivotaltracker.com/story/show/168798311

--- selenium-test/page-objects/landing-page.js
+++ selenium-test/page-objects/landing-page.js
@@ -7,85 +7,65 @@ import Footer from './sections/footer';
 import RichTextToolbar from './sections/rich-text-toolbar';
 
 
-class CollaborateSection extends Section {
-  constructor() {
-    super();
+class CarouselSection extends Section {
+  constructor(parentSelector='', mainElementSelector, cardSelector) {
+    super(parentSelector, mainElementSelector);
     this.prepareElementGetters({
-      editToggle: '//div[@class="test--collaborate-section"]//a[@class="test--more-link"]',
-      headerTitle: '//*[@class="test--collaborate-section-header"]//div[@data-block="true"]',
-      cancelButton: '//div[@class="test--collaborate-section"]//a[contains(@class, "cancel-button")]',
-      updateButton: '//div[@class="test--collaborate-section"]//a[contains(@class, "update-button")]',
-      content: '//div[@class="test--collaborate-section-content"]//div[@data-contents="true"]',
+      leftArrow: '//*[contains(@class, "test--carousel-arrow-left")]',
+      rightArrow: '//*[contains(@class, "test--carousel-arrow-right")]',
+      firstPinButton: `${cardSelector}//div[contains(@class, "item-pin-button__item-pin-button")]`,
+      cards: cardSelector,
     });
   }
 }
 
-class RecentActivityCarouselSection extends Section {
+class RecentActivityCarouselSection extends CarouselSection {
   constructor() {
-    super();
+    super(
+      '',
+      '//div[@class="test--landing-carousel-activity landing-page-carousel"]',
+      '//a[contains(@class, "officer-card")]'
+    );
     this.prepareElementGetters({
-      leftArrow: '//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-left")]',
-      rightArrow: '//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-right")]',
-      officerCards: '//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//a[contains(@class, "officer-card")]',
-      pairCards: '//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//div[contains(@class, "pairing-card__pairing-card")]',
-      firstPairCard:
-        '(//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//div[contains(@class, "pairing-card__pairing-card")])[1]',
-      firstPairCardLeftHalf: '' +
-        '((//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//div[contains(@class, "pairing-card__pairing-card")])[1]' +
-        '//a[contains(@class, "half-pane")])[1]',
-      firstPairCardRightHalf: '' +
-        '((//div[@class="test--landing-carousel-activity landing-page-carousel"]' +
-        '//div[contains(@class, "pairing-card__pairing-card")])[1]' +
-        '//a[contains(@class, "half-pane")])[2]',
+      pairCards: '//div[contains(@class, "pairing-card__pairing-card")]',
+      firstPairCard: '(//div[contains(@class, "pairing-card__pairing-card")])[1]',
+      firstPairCardLeftHalf: '(//div[contains(@class, "pairing-card__pairing-card")])[1]' +
+        '//a[contains(@class, "half-pane")][1]',
+      firstPairCardRightHalf: '(//div[contains(@class, "pairing-card__pairing-card")])[1]' +
+        '//a[contains(@class, "half-pane")][2]',
+      firstPairCardPinButton: '(//div[contains(@class, "pairing-card__pairing-card")])[1]' +
+        '//div[contains(@class, "item-pin-button__item-pin-button")]',
     });
   }
 }
 
-class OfficersByAllegationCarouselSection extends Section {
+class OfficersByAllegationCarouselSection extends CarouselSection {
   constructor() {
-    super();
-    this.prepareElementGetters({
-      leftArrow: '//div[@class="test--landing-carousel-allegation landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-left")]',
-      rightArrow: '//div[@class="test--landing-carousel-allegation landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-right")]',
-      cards: '//div[@class="test--landing-carousel-allegation landing-page-carousel"]' +
-        '//a[contains(@class, "officer-card")]',
-    });
+    super(
+      '',
+      '//div[@class="test--landing-carousel-allegation landing-page-carousel"]',
+      '//a[contains(@class, "officer-card")]',
+    );
   }
 }
 
-class RecentDocumentCarouselSection extends Section {
+class RecentDocumentCarouselSection extends CarouselSection {
   constructor() {
-    super();
-    this.prepareElementGetters({
-      leftArrow: '//div[@class="test--landing-carousel-document landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-left")]',
-      rightArrow: '//div[@class="test--landing-carousel-document landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-right")]',
-      cards: '//div[@class="test--landing-carousel-document landing-page-carousel"]' +
-        '//a[contains(@class, "document-card__document-card")]',
-    });
+    super(
+      '',
+      '//div[@class="test--landing-carousel-document landing-page-carousel"]',
+      '//a[contains(@class, "document-card__document-card")]'
+    );
   }
 }
 
-class ComplaintSummariesCarouselSection extends Section {
+class ComplaintSummariesCarouselSection extends CarouselSection {
   constructor() {
-    super();
-    this.prepareElementGetters({
-      leftArrow: '//div[@class="test--landing-carousel-complaint landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-left")]',
-      rightArrow: '//div[@class="test--landing-carousel-complaint landing-page-carousel"]' +
-        '//*[contains(@class, "test--carousel-arrow-right")]',
-      cards: '//div[@class="test--landing-carousel-complaint landing-page-carousel"]' +
-        '//a[contains(@class, "complaint-summary-card")]',
-    });
+    super(
+      '',
+      '//div[@class="test--landing-carousel-complaint landing-page-carousel"]',
+      '//a[contains(@class, "complaint-summary-card")]',
+    );
   }
 }
 
@@ -166,7 +146,6 @@ class LandingPage extends Page {
   header = new Header();
   footer = new Footer();
   richTextToolbar = new RichTextToolbar();
-  collaborateSection = new CollaborateSection();
   recentActivityCarousel = new RecentActivityCarouselSection();
   officersByAllegationCarousel = new OfficersByAllegationCarouselSection();
   complaintSummariesCarousel = new ComplaintSummariesCarouselSection();
@@ -174,6 +153,15 @@ class LandingPage extends Page {
   genericModalSection = new GenericModalSection();
   heatMapSection = new HeatMapSection();
   searchSection = new SearchSection();
+
+  constructor() {
+    super();
+    this.prepareElementGetters({
+      toast: '.Toastify__toast-body',
+      lastToast: '(//div[contains(@class, "Toastify__toast-body")])[last()]',
+      secondLastToast: '(//div[contains(@class, "Toastify__toast-body")])[last() - 1]',
+    });
+  }
 
   open() {
     super.open('/');
