1bf53377aa04201bb8063ee3cd2041f890ea3a0c
ea-open-source <open-source@eastagile.com>
2020-02-21 11:40:14 +0700

    [f] Display pairing card at 1st position in twitter section

--- src/js/selectors/landing-page/activity-grid.js
+++ src/js/selectors/landing-page/activity-grid.js
@@ -1,11 +1,29 @@
 import { createSelector } from 'reselect';
+import { filter, range, sample, isUndefined } from 'lodash';
 
 import { cardTransform, shuffled } from './common';
-import { PINNED_ITEM_TYPES } from 'utils/constants';
+import { PINNED_ITEM_TYPES, ACTIVITY_GRID_CARD_TYPES } from 'utils/constants';
 import { createWithIsPinnedSelector } from 'selectors/common/pinboard';
 
 
 const getCards = state => state.landingPage.activityGrid.cards;
+
+const selectRandomPairingCard = (cards) => {
+  const paringCardIndices = filter(range(cards.length), (index) => cards[index].kind === ACTIVITY_GRID_CARD_TYPES.PAIR);
+  return sample(paringCardIndices);
+};
+
+const moveParingCardToBeginning = (selector) => createSelector(
+  selector,
+  cards => {
+    const pairingCardIndex = selectRandomPairingCard(cards);
+    if (isUndefined(pairingCardIndex) || pairingCardIndex === 0) {
+      return cards;
+    }
+    return [cards[pairingCardIndex], ...filter(cards, (_, index) => index !== pairingCardIndex )];
+  }
+);
+
 export const getCarouselActivityHeaderEditModeOn = state => state.landingPage.activityGrid.headerEditModeOn;
 
 export const hasCards = createSelector(
@@ -14,7 +32,7 @@ export const hasCards = createSelector(
 );
 
 export const cardsSelector = createWithIsPinnedSelector(
-  shuffled(getCards),
+  moveParingCardToBeginning(shuffled(getCards)),
   PINNED_ITEM_TYPES.OFFICER,
   cardTransform,
 );
--- test/selectors/landing-page/activity-grid.js
+++ test/selectors/landing-page/activity-grid.js
@@ -1,9 +1,17 @@
 import lodash from 'lodash';
 import { spy } from 'sinon';
 
+import { pairingCardTransform } from 'selectors/landing-page/common';
 import { cardsSelector, hasCards } from 'selectors/landing-page/activity-grid';
 import { RawOfficerCardFactory } from 'utils/test/factories/activity-grid.js';
 
+
+const pinnedPairingCardTransform = (pairingCard) => {
+  const transformedCard = pairingCardTransform(pairingCard);
+  transformedCard.officer1['isPinned'] = false;
+  transformedCard.officer2['isPinned'] = false;
+  return transformedCard;
+};
 
 describe('activity-grid selectors', function () {
   let state;
@@ -95,6 +103,161 @@ describe('activity-grid selectors', function () {
 
       stubShuffle.calledWith(lodash.range(0, 12)).should.be.true();
       stubShuffle.calledWith(lodash.range(12, 40)).should.be.true();
+    });
+
+    it('should return pairing card in the 1st position', function () {
+      state.landingPage.activityGrid.cards = [{
+        id: 16473,
+        'full_name': 'Robert Longfellow',
+        'complaint_count': 0,
+        'sustained_count': 0,
+        'birth_year': 1924,
+        'complaint_percentile': null,
+        race: 'White',
+        gender: 'Male',
+        rank: 'Police Officer / Mounted Patrol Officer',
+        percentile: {
+          year: 1984,
+        },
+        kind: 'single_officer',
+      }, {
+        id: 13788,
+        'full_name': 'Broderick Jones',
+        'complaint_count': 107,
+        'sustained_count': 11,
+        'birth_year': 1971,
+        'complaint_percentile': 99.9911,
+        race: 'Black',
+        gender: 'Male',
+        rank: 'Police Officer',
+        percentile: {
+          year: 1999,
+        },
+        kind: 'single_officer',
+      }, {
+        officer1: {
+          id: 13937,
+          'full_name': 'Jaeho Jung',
+          'birth_year': 1981,
+          race: 'Asian/Pacific',
+          gender: 'Male',
+          percentile: {
+            year: 2016,
+          },
+          rank: 'Police Officer',
+          'complaint_count': 61,
+          'sustained_count': 1,
+        },
+        officer2: {
+          id: 25602,
+          'full_name': 'Matthew Schaller',
+          'birth_year': 1983,
+          race: 'White',
+          gender: 'Male',
+          percentile: {
+            year: 2017,
+          },
+          rank: 'Sergeant of Police',
+          'complaint_count': 27,
+          'sustained_count': 0,
+        },
+        'coaccusal_count': 0,
+        kind: 'coaccused_pair',
+      }, {
+        officer1: {
+          id: 13421,
+          'full_name': 'Melinda May',
+          'birth_year': 1990,
+          race: 'Asian/Pacific',
+          gender: 'Male',
+          percentile: {
+            year: 2013,
+          },
+          rank: 'Police Officer',
+          'complaint_count': 30,
+          'sustained_count': 1,
+        },
+        officer2: {
+          id: 47383,
+          'full_name': 'Phil Coulson',
+          'birth_year': 1988,
+          race: 'White',
+          gender: 'Male',
+          percentile: {
+            year: 2011,
+          },
+          rank: 'Sergeant of Police',
+          'complaint_count': 11,
+          'sustained_count': 0,
+        },
+        'coaccusal_count': 9,
+        kind: 'coaccused_pair',
+      }];
+      const cards = cardsSelector(state);
+      const pairCards = [pinnedPairingCardTransform({
+        officer1: {
+          id: 13937,
+          'full_name': 'Jaeho Jung',
+          'birth_year': 1981,
+          race: 'Asian/Pacific',
+          gender: 'Male',
+          percentile: {
+            year: 2016,
+          },
+          rank: 'Police Officer',
+          'complaint_count': 61,
+          'sustained_count': 1,
+        },
+        officer2: {
+          id: 25602,
+          'full_name': 'Matthew Schaller',
+          'birth_year': 1983,
+          race: 'White',
+          gender: 'Male',
+          percentile: {
+            year: 2017,
+          },
+          rank: 'Sergeant of Police',
+          'complaint_count': 27,
+          'sustained_count': 0,
+          isPinned: false,
+        },
+        'coaccusal_count': 0,
+        kind: 'coaccused_pair',
+      }), pinnedPairingCardTransform({
+        officer1: {
+          id: 13421,
+          'full_name': 'Melinda May',
+          'birth_year': 1990,
+          race: 'Asian/Pacific',
+          gender: 'Male',
+          percentile: {
+            year: 2013,
+          },
+          rank: 'Police Officer',
+          'complaint_count': 30,
+          'sustained_count': 1,
+          isPinned: false,
+        },
+        officer2: {
+          id: 47383,
+          'full_name': 'Phil Coulson',
+          'birth_year': 1988,
+          race: 'White',
+          gender: 'Male',
+          percentile: {
+            year: 2011,
+          },
+          rank: 'Sergeant of Police',
+          'complaint_count': 11,
+          'sustained_count': 0,
+          isPinned: false,
+        },
+        'coaccusal_count': 9,
+        kind: 'coaccused_pair',
+      })];
+      cards.should.have.length(4);
+      lodash.some(pairCards, cards[0]).should.be.true();
     });
   });
 
