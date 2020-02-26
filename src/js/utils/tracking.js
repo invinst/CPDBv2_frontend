ad799fc5d376e99538be2b12f37b562abb5fb625
ea-open-source <open-source@eastagile.com>
2020-02-26 18:16:55 +0700

    [f] Analytic tracking server side when uBlock origin is enabled
    [171280318](https://www.pivotaltracker.com/story/show/171280318)

--- src/js/utils/tracking.js
+++ src/js/utils/tracking.js
@@ -1,31 +1,87 @@
-import { throttle } from 'lodash';
+import { throttle, isEmpty, get, pick } from 'lodash';
+
+import axiosClient from 'utils/axios-client';
+import { V2_ROOT_PATH } from 'utils/constants';
 
 
-const clickyLog = (title, type) => global.clicky.log(document.location.pathname, title, type);
+const analyticTrackingParams = ({ clickyData, gaData }) => {
+  let trackingParams = {};
+  if (!isEmpty(clickyData)) {
+    trackingParams['clicky'] = pick(clickyData, ['type', 'href', 'title']);
+  }
+  if (!isEmpty(gaData)) {
+    trackingParams['ga'] = {
+      'hit_type': gaData.hitType,
+      'event_category': gaData.eventCategory,
+      'event_action': gaData.eventAction,
+      'event_label': gaData.eventLabel,
+      'event_value': gaData.eventValue,
+      'page': gaData.page,
+    };
+  }
+  return trackingParams;
+};
+
+const sendAnalyticTracking = (data) => {
+  const callback = get(data, 'gaData.hitCallback');
+  axiosClient.post(`${V2_ROOT_PATH}tracking/`, analyticTrackingParams(data))
+    .finally(() => {
+      callback && callback();
+    });
+};
+
+const analyticTracking = ({ clickyData, gaData }) => {
+  let serverSideTrackingData = {};
+  clickyData['href'] = document.location.pathname;
+  if (!global.clicky) {
+    serverSideTrackingData['clickyData'] = clickyData;
+  }
+  if (!global.gaLoaded) {
+    serverSideTrackingData['gaData'] = gaData;
+  }
+
+  if (!isEmpty(serverSideTrackingData)) {
+    sendAnalyticTracking(serverSideTrackingData);
+  }
+
+  if (global.clicky && !clickyData.defaultTracking) {
+    global.clicky.log(clickyData.href, clickyData.title, clickyData.type);
+  }
+  global.gaLoaded && global.ga('send', gaData);
+};
 
 export const trackSwipeLandingPageCarousel = (direction, type) => {
-  clickyLog(`swipe_${direction}_${type}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'landing_page_carousel',
-    eventAction: `swipe_${direction}`,
-    eventLabel: type,
+  analyticTracking({
+    clickyData: {
+      title: `swipe_${direction}_${type}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'landing_page_carousel',
+      eventAction: `swipe_${direction}`,
+      eventLabel: type,
+    },
   });
 };
 
 export const trackOutboundLink = (url, windowName) => {
-  clickyLog(url, 'outbound');
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'outbound',
-    eventAction: 'click',
-    eventLabel: url,
-    transport: 'beacon',
-    hitCallback: () => {
-      /* istanbul ignore next */
-      if (!windowName) {
-        document.location = url;
-      }
+  analyticTracking({
+    clickyData: {
+      title: url,
+      type: 'outbound',
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'outbound',
+      eventAction: 'click',
+      eventLabel: url,
+      transport: 'beacon',
+      hitCallback: () => {
+        /* istanbul ignore next */
+        if (!windowName) {
+          document.location = url;
+        }
+      },
     },
   });
 
@@ -35,154 +91,216 @@ export const trackOutboundLink = (url, windowName) => {
 };
 
 export const trackPageView = (pathname) => {
-  global.ga('send', {
-    hitType: 'pageview',
-    page: pathname,
+  analyticTracking({
+    clickyData: {
+      type: 'pageview',
+      defaultTracking: true,
+    },
+    gaData: {
+      hitType: 'pageview',
+      page: pathname,
+    },
   });
 };
 
 export const trackSearchResultsCount = (count) => {
-  clickyLog(`num_results: ${count}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'search',
-    eventAction: 'num_results',
-    eventValue: count,
+  analyticTracking({
+    clickyData: {
+      title: `num_results: ${count}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'search',
+      eventAction: 'num_results',
+      eventValue: count,
+    },
   });
 };
 
 export const trackSingleSearchResults = (contentType, query, resultsCount) => {
-  clickyLog(`single_search_query: ${query} with ${resultsCount} results`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: contentType,
-    eventAction: 'single_search',
-    eventLabel: query,
-    eventValue: resultsCount,
+  analyticTracking({
+    clickyData: {
+      title: `single_search_query: ${query} with ${resultsCount} results`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: contentType,
+      eventAction: 'single_search',
+      eventLabel: query,
+      eventValue: resultsCount,
+    },
   });
 };
 
 const _trackSearchFocusedItem = (contentType, query, itemId, rank) => {
-  clickyLog(`Item ${itemId} with rank ${rank} is focused`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: contentType,
-    eventAction: 'suggestion_click',
-    eventLabel: itemId,
-    eventValue: rank,
+  analyticTracking({
+    clickyData: {
+      title: `Item ${itemId} with rank ${rank} is focused`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: contentType,
+      eventAction: 'suggestion_click',
+      eventLabel: itemId,
+      eventValue: rank,
+    },
   });
-
-  clickyLog(`Item ${itemId} with rank ${rank} is focused via "${query}" query`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: contentType,
-    eventAction: 'suggestion_click_with_query',
-    eventLabel: `${ itemId } - ${ query }`,
-    eventValue: rank,
+  analyticTracking({
+    clickyData: {
+      title: `Item ${itemId} with rank ${rank} is focused via "${query}" query`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: contentType,
+      eventAction: 'suggestion_click_with_query',
+      eventLabel: `${ itemId } - ${ query }`,
+      eventValue: rank,
+    },
   });
 };
 
 export const trackSearchFocusedItem = throttle(_trackSearchFocusedItem, 500, { 'leading': false });
 
 function _trackSearchQuery(query) {
-  clickyLog(`change_query: ${query}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'search',
-    eventAction: 'change_query',
-    eventLabel: query,
+  analyticTracking({
+    clickyData: {
+      title: `change_query: ${query}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'search',
+      eventAction: 'change_query',
+      eventLabel: query,
+    },
   });
 }
 
 export const trackSearchQuery = throttle(_trackSearchQuery, 500, { 'leading': false });
 
 export const trackCommunityClick = (communityName) => {
-  clickyLog(`community: ${communityName}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'community',
-    eventAction: 'click',
-    eventLabel: communityName,
+  analyticTracking({
+    clickyData: {
+      title: `community: ${communityName}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'community',
+      eventAction: 'click',
+      eventLabel: communityName,
+    },
   });
 };
 
 export const trackOpenExplainer = (officerId) => {
-  clickyLog(`open_visual_token_explainer: ${officerId}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'visual_token_explainer',
-    eventAction: 'open',
-    eventValue: officerId,
+  analyticTracking({
+    clickyData: {
+      title: `open_visual_token_explainer: ${officerId}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'visual_token_explainer',
+      eventAction: 'open',
+      eventValue: officerId,
+    },
   });
 };
 
 export const trackOfficerDownload = (officerId, action, label) => {
-  clickyLog(`officer_data: ${officerId}`, 'download');
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'officer_data',
-    eventAction: action,
-    eventValue: officerId,
-    eventLabel: label,
+  analyticTracking({
+    clickyData: {
+      title: `officer_data: ${officerId}`,
+      type: 'download',
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'officer_data',
+      eventAction: action,
+      eventValue: officerId,
+      eventLabel: label,
+    },
   });
 };
 
 export const trackOfficerDownloadMenu = (officerId, action) => {
-  clickyLog(`open_officer_download_menu: ${officerId}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'officer_download_menu',
-    eventAction: action,
-    eventValue: officerId,
+  analyticTracking({
+    clickyData: {
+      title: `open_officer_download_menu: ${officerId}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'officer_download_menu',
+      eventAction: action,
+      eventValue: officerId,
+    },
   });
 };
 
 export const trackRelatedByCategoryClick = (sourceCRID, targetCRID) => {
-  clickyLog(`related_by_category: Source CRID ${sourceCRID} - Target CRID ${targetCRID}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'related_by_category',
-    eventAction: 'click',
-    eventLabel: `Source CRID: ${sourceCRID} - Target CRID: ${targetCRID}`,
+  analyticTracking({
+    clickyData: {
+      title: `related_by_category: Source CRID ${sourceCRID} - Target CRID ${targetCRID}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'related_by_category',
+      eventAction: 'click',
+      eventLabel: `Source CRID: ${sourceCRID} - Target CRID: ${targetCRID}`,
+    },
   });
 };
 
 export const trackRelatedByAccusedClick = (sourceCRID, targetCRID) => {
-  clickyLog(`related_by_accused: Source CRID ${sourceCRID} - Target CRID ${targetCRID}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'related_by_accused',
-    eventAction: 'click',
-    eventLabel: `Source CRID: ${sourceCRID} - Target CRID: ${targetCRID}`,
+  analyticTracking({
+    clickyData: {
+      title: `related_by_accused: Source CRID ${sourceCRID} - Target CRID ${targetCRID}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'related_by_accused',
+      eventAction: 'click',
+      eventLabel: `Source CRID: ${sourceCRID} - Target CRID: ${targetCRID}`,
+    },
   });
 };
 
 export const trackAttachmentClick = (sourceUrl, targetUrl) => {
-  clickyLog(`attachment_click: Source URL ${sourceUrl} - Target URL ${targetUrl}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'attachment_click',
-    eventAction: 'click',
-    eventLabel: `Source URL: ${sourceUrl} - Target URL: ${targetUrl}`,
+  analyticTracking({
+    clickyData: {
+      title: `attachment_click: Source URL ${sourceUrl} - Target URL ${targetUrl}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'attachment_click',
+      eventAction: 'click',
+      eventLabel: `Source URL: ${sourceUrl} - Target URL: ${targetUrl}`,
+    },
   });
 };
 
 export const trackPopupButtonClick = (sourceUrl, popupName) => {
-  clickyLog(`popup_click: ${sourceUrl} - ${popupName}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'popup_click',
-    eventAction: 'click',
-    eventLabel: `${sourceUrl} - ${popupName}`,
+  analyticTracking({
+    clickyData: {
+      title: `popup_click: ${sourceUrl} - ${popupName}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'popup_click',
+      eventAction: 'click',
+      eventLabel: `${sourceUrl} - ${popupName}`,
+    },
   });
 };
 
 export const trackDocumentEdit = (documentID, documentField) => {
-  clickyLog(`document_edit: Document ID ${documentID} - Field ${documentField}`);
-  global.ga('send', {
-    hitType: 'event',
-    eventCategory: 'document_edit',
-    eventAction: 'edit',
-    eventLabel: `Document ID: ${documentID} - Field: ${documentField}`,
+  analyticTracking({
+    clickyData: {
+      title: `document_edit: Document ID ${documentID} - Field ${documentField}`,
+    },
+    gaData: {
+      hitType: 'event',
+      eventCategory: 'document_edit',
+      eventAction: 'edit',
+      eventLabel: `Document ID: ${documentID} - Field: ${documentField}`,
+    },
   });
 };
