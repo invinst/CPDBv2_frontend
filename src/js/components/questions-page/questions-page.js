05aec625f99d40242d3f2ed48ddf0a2e5e00f4a1
Maheen Khan <maheenkhan@Maheens-MacBook-Pro.local>
2022-02-18 16:10:28 -0600

    added basic stuff to page

--- src/js/components/questions-page/questions-page.js
+++ src/js/components/questions-page/questions-page.js
@@ -4,8 +4,8 @@ import CopyLinkButton from 'components/common/copy-link-btn';
 import ResponsiveStyleComponent from 'components/responsive/responsive-style-component';
 import {
   wrapperStyle, headLineStyle, leftColumnStyle, rightColumnStyle,
-  columnHeadLineStyle, emailLinkStyle, listElementStyle, paragraphStyle,
-  emailLinkWrapperStyle, smallTextStyle, copyLinkStyle, listStyle,
+  sectionHeaderStyle, emailLinkStyle, listElementStyle, paragraphStyle,
+  emailLinkWrapperStyle, smallTextStyle, copyLinkStyle, listStyle, smallSubHeaderStyle,
 } from './questions-page.style';
 import { MOBILE, TABLET, DESKTOP } from 'utils/constants';
 import NoRerender from 'components/common/higher-order/no-rerender';
@@ -23,9 +23,61 @@ class QuestionsPage extends Component {
         </div>
         
         <div>
-          <h2 style={ columnHeadLineStyle }>
+          <h2 style={ sectionHeaderStyle }>
             The Complaint Process
           </h2>
+          <h3 style={ smallSubHeaderStyle }>
+            Can you help me file a complaint? How do complaints get investigated?
+            <br></br>How do I file a complaint? Why is this information imperfect?
+          </h3>
+        </div>
+
+        <div>
+          <h2 style={ sectionHeaderStyle }>
+            About the Data
+          </h2>
+          <h3 style={ smallSubHeaderStyle }>
+            How did this data become public? How current is the data? How accurate is the data?
+            <br></br>How do I report incorrect data? Can I download the whole dataset?
+          </h3>
+        </div>
+
+        <div>
+          <h2 style={ sectionHeaderStyle }>
+            How does CPDP work?
+          </h2>
+          <h3 style={ smallSubHeaderStyle }>
+            Who made this website? How does CPDP get its data? <br></br> How are the datasets organized?
+          </h3>
+        </div>
+
+        <div>
+          <h2 style={ sectionHeaderStyle }>
+            What can I search?
+          </h2>
+          <h3 style={ smallSubHeaderStyle }>
+            How do I search for someone? Can I search by race,<br></br>or gender, or other categories?
+            How are your percentiles calculated?
+          </h3>
+        </div>
+
+        <div>
+          <h2 style={ sectionHeaderStyle }>
+            Glossary
+          </h2>
+          <h3 style={ smallSubHeaderStyle }>
+            What is FOIA, TRR, CR, IPRA, and COPA? Click "glossary" and "what can I search"<br></br> on the CPDP home page for more definitions.
+          </h3>
+        </div>
+
+        <div>
+          <h2 style={ sectionHeaderStyle }>
+            Collaborate
+          </h2>
+          <h3 style={ smallSubHeaderStyle }>
+            How can I collaborate with CPDP and the Invisible Institute? Where is the information I requested?
+            <br></br>If I have information that you don't have, how can I share it?
+          </h3>
         </div>
       </div>
       
--- src/js/components/questions-page/questions-page.style.js
+++ src/js/components/questions-page/questions-page.style.js
@@ -1,4 +1,4 @@
-import { whiteTwoColor, softBlackColor, accentColor, lightGreyColor, sanFranciscoTextFamily, minionProFamily } from 'utils/styles';
+import { smokeGray, softBlackColor, sanFranciscoTextFamily, minionProFamily } from 'utils/styles';
 
 
 export const wrapperStyle = {
@@ -26,71 +26,22 @@ export const headLineStyle = {
   height: '90px',
 };
 
-export const leftColumnStyle = {
-  paddingRight: '32px',
-  height: '354px',
-  borderRight: `1px solid ${lightGreyColor}`,
-  boxSizing: 'border-box',
-};
-
-export const rightColumnStyle = {
-  paddingLeft: '32px',
-  boxSizing: 'border-box',
-};
-
-export const columnHeadLineStyle = {
-  height: '53px',
+export const sectionHeaderStyle = {
   margin: '0',
-  padding: '0 0 35px 0',
   color: softBlackColor,
-  fontSize: '20px',
+  fontSize: '19px',
   fontFamily: sanFranciscoTextFamily,
   fontWeight: '600',
   boxSizing: 'border-box',
   textAlign: 'center',
 };
 
-export const emailLinkWrapperStyle = {
-  height: '62px',
-  padding: '17px 0',
-  boxSizing: 'border-box',
-};
-
-export const emailLinkStyle = {
-  color: accentColor,
-  fontSize: '16px',
+export const smallSubHeaderStyle = {
+  color: smokeGray,
+  fontSize: '14px',
   fontFamily: sanFranciscoTextFamily,
-  fontWeight: '600',
-  textDecoration: 'none',
-  verticalAlign: 'middle',
-};
-
-export const copyLinkStyle = {
-  marginLeft: '16px',
-  verticalAlign: 'middle',
-};
-
-export const listElementStyle = {
-  color: softBlackColor,
-  fontSize: '13px',
-  fontFamily: sanFranciscoTextFamily,
-  fontWeight: '600',
-};
-
-export const smallTextStyle = {
-  color: softBlackColor,
-  fontSize: '13px',
-  fontFamily: sanFranciscoTextFamily,
-};
-
-export const paragraphStyle = {
-  fontFamily: minionProFamily,
   fontWeight: 'normal',
-  fontSize: '19px',
-  margin: '0 0 9px 0',
-  color: softBlackColor,
-};
-
-export const listStyle = {
-  paddingLeft: '14px',
+  textDecoration: 'none',
+  textAlign: 'center',
+  paddingBottom: '30px',
 };
