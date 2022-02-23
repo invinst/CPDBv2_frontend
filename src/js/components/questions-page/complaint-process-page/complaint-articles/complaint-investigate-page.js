4e29505ed754569f6ab9d273c5a878475eae3ccd
Maheen Khan <maheenkhan@Maheens-MacBook-Pro.local>
2022-02-23 10:11:45 -0600

    complaint page fixes

--- src/js/components/questions-page/complaint-process-page/complaint-articles/complaint-investigate-page.js
+++ src/js/components/questions-page/complaint-process-page/complaint-articles/complaint-investigate-page.js
@@ -30,7 +30,7 @@ class ComplaintInvestigatePage extends React.PureComponent {
             Police officers, including excessive force, domestic violence, and improper search<br/>
             and seizure. COPA also investigates all officer-involved shootings. All other <br/>
             complaints are investigated by CPD officers, typically police supervisors or officers<br/>
-            assigned to the <a href='https://home.chicagopolice.org/inside-cpd/reports/' target='_blank' 
+            assigned to the <a href='https://home.chicagopolice.org/inside-cpd/reports/' target='_blank'
               rel='noopener noreferrer'><u>Bureau of Internal Affairs</u></a>. <br/>
             <br/>The implicated officers will be notified of the complaint. The investigators will collect<br/>
             evidence, conduct interviews, and prepare their findings. A complaint can either be<br/>
@@ -39,7 +39,8 @@ class ComplaintInvestigatePage extends React.PureComponent {
             The <a href='https://www.chicago.gov/city/en/depts/cpb.html' target='_blank' rel='noopener noreferrer'>
               <u>Chicago Police Board</u></a> is the agency that makes the final decision about whether<br/>
             to discipline an officer. <br/><br/>
-            On COPA's website, there is a detailed explanation of the <a href='https://www.chicagocopa.org/investigations/investigative-process/' 
+            On COPA's website, there is a detailed explanation of the
+            <a href='https://www.chicagocopa.org/investigations/investigative-process/'
               target='_blank' rel='noopener noreferrer'><u>Investigative Process</u></a>.
           </p>
         </div>
