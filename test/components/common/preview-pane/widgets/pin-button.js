483780be4bb7033d55422bda3e313a799e19c9db
Maheen Khan <maheen@invisibleinstitute.com>
2024-08-09 16:44:36 -0500

    commenting out pinboard button

--- test/components/common/preview-pane/widgets/pin-button.js
+++ test/components/common/preview-pane/widgets/pin-button.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { stub } from 'sinon';
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import { stub } from 'sinon';
 
-import PinButton from 'components/common/preview-pane/widgets/pin-button';
+// import PinButton from 'components/common/preview-pane/widgets/pin-button';
 
 
 describe('PinButton component', () => {
-  it('should display "Remove from pinboard" if isPinned', function () {
-    const wrapper = mount(
-      <PinButton item={ { isPinned: true } } />
-    );
+  // it('should display "Remove from pinboard" if isPinned', function () {
+  //   const wrapper = mount(
+  //     <PinButton item={ { isPinned: true } } />
+  //   );
 
-    wrapper.find('button').text().should.equal('Remove from pinboard');
-  });
+  //   wrapper.find('button').text().should.equal('Remove from pinboard');
+  // });
 
-  it('should display "Add to pinboard" if not isPinned', function () {
-    const wrapper = mount(
-      <PinButton item={ { isPinned: false } } />
-    );
+  // it('should display "Add to pinboard" if not isPinned', function () {
+  //   const wrapper = mount(
+  //     <PinButton item={ { isPinned: false } } />
+  //   );
 
-    wrapper.find('button').text().should.equal('Add to pinboard');
-  });
+  //   wrapper.find('button').text().should.equal('Add to pinboard');
+  // });
 
-  it('should handle addOrRemoveItemInPinboard on click', function () {
-    const addOrRemoveItemInPinboardStub = stub();
-    const wrapper = mount(
-      <PinButton
-        addOrRemoveItemInPinboard={ addOrRemoveItemInPinboardStub }
-        item={ { type: 'OFFICER', id: 123456, isPinned: true } }
-      />
-    );
+  // it('should handle addOrRemoveItemInPinboard on click', function () {
+  //   const addOrRemoveItemInPinboardStub = stub();
+  //   const wrapper = mount(
+  //     <PinButton
+  //       addOrRemoveItemInPinboard={ addOrRemoveItemInPinboardStub }
+  //       item={ { type: 'OFFICER', id: 123456, isPinned: true } }
+  //     />
+  //   );
 
-    const pinButton = wrapper.find('button');
+  //   const pinButton = wrapper.find('button');
 
-    pinButton.simulate('click');
+  //   pinButton.simulate('click');
 
-    addOrRemoveItemInPinboardStub.calledWith({
-      type: 'OFFICER',
-      id: 123456,
-      isPinned: true,
-    });
-  });
+  //   addOrRemoveItemInPinboardStub.calledWith({
+  //     type: 'OFFICER',
+  //     id: 123456,
+  //     isPinned: true,
+  //   });
+  // });
 });
